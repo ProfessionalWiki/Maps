08b009cac1d5abb152154b01ea5dbf340d2f72bc
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-31 19:36:32 +0000

    Localisation updates for extension messages from translatewiki.net (2009-07-31 17:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@54118 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.i18n.php
+++ Maps.i18n.php
@@ -19,9 +19,95 @@ $messages['en'] = array(
 	'maps_desc' => 'Provides the ability to display coordinate data in maps, and geocode addresses. Available mapping services: $1',
 );
 
-/** Dutch (Nederlands)
- * @author Jeroen De Dauw
+/** Message documentation (Message documentation)
+ * @author Raymond
  */
+$messages['qqq'] = array(
+	'maps_name' => '{{Optional}}',
+	'maps_desc' => '{{desc}}
+
+* $1: a list of available map services',
+);
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author EugeneZelenko
+ */
+$messages['be-tarask'] = array(
+	'maps_desc' => 'Забясьпечвае магчымасьць адлюстраваньня каардынатных зьвестак на мапах і геаграфічнага кадаваньня адрасоў. Даступныя геаграфічныя сэрвісы: $1',
+);
+
+/** Bosnian (Bosanski)
+ * @author CERminator
+ */
+$messages['bs'] = array(
+	'maps_desc' => 'Daje mogućnost prikazivanja podataka koordinata na mapama i geocode adresa. Dostupne usluge mapa: $1',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'maps_desc' => 'Bitujo móžnosć koordinatowe daty w geografiskich kórtach a geokodowe adrese zwobrazniś. K dispoziciji stojece kórtowe słužby: $1',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'maps_desc' => 'Proporciona a capacidade de mostrar datos de coordenadas en mapas, e enderezos xeocodificados. Servizos de mapeamento dispoñibles: $1',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'maps_desc' => 'Skići móžnosć koordinatowe daty w geografiskich kartach a geokodne adresy zwobraznić. K dispoziciji stejace kartowe słužby: $1',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author Bennylin
+ */
+$messages['id'] = array(
+	'maps_desc' => "Memampukan tampilan data koordinat pada peta, dan alamat ''geocode''. Layanan pemetaan yang tersedia: $1",
+);
+
+/** Japanese (日本語)
+ * @author Fryed-peach
+ */
+$messages['ja'] = array(
+	'maps_desc' => '地図上に座標データを表示し、住所を座標データに変換する機能を提供する。次の地図サービスに対応します：$1',
+);
+
+/** Ripoarisch (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'maps_desc' => 'Määt et müjjelesch, Koodinaate en Landkaate aanzezeije, un Addräße en Koodinaate op de Ääd ömzerääschne. He di Deenste för Landkaat ham_mer ze beede: $1',
+);
+
+/** Dutch (Nederlands) */
 $messages['nl'] = array(
 	'maps_desc' => 'Bied de mogelijkheid om locatie gegevens weer te geven op mappen en het omzetten van addressen naar coordinaten. Provides the ability to display coordinate data in maps, and geocode addresses. Beschikbare mapping services: $1',
 );
+
+/** Tarandíne (Tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'maps_desc' => "Dè l'abbilità a fà vedè le coordinate jndr'à le mappe e le indirizze geocodificate. Disponibbile le servizie de mappe: $1",
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'maps_desc' => 'Обеспечивает возможность отображения координатных данных на картах геокодирование адресов. Доступные картографические службы: $1',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'maps_desc' => 'Poskytuje možnosť zobrazovať údaje súradníc na mapách a tvoriť geografické adresy lokalít. Dostupné mapovacie služby: $1',
+);
+
