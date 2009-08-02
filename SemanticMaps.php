0ed56036d2d931bcd5d49da85e6001bb785d3392
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-02 18:06:50 +0000

    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticMaps@54219 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticMaps.php
+++ SemanticMaps.php
@@ -1,5 +1,5 @@
 <?php
-
+  
 /**
  * Initialization file for the Semantic Maps extension.
  * Extension documentation: http://www.mediawiki.org/wiki/Extension:Semantic_Maps
@@ -14,7 +14,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define('SM_VERSION', '0.2');
+define('SM_VERSION', '0.3');
 
 $smgScriptPath = $wgScriptPath . '/extensions/SemanticMaps';
 $smgIP = $IP . '/extensions/SemanticMaps';
@@ -30,10 +30,18 @@ $wgAutoloadClasses['SMMapPrinter'] = $smgIP . '/SM_MapPrinter.php';
 $wgAutoloadClasses['SMMapper'] = $smgIP . '/SM_Mapper.php';
 $wgAutoloadClasses['SMFormInput'] = $smgIP . '/SM_FormInput.php';
 
-function smfSetup() {
-	global $wgExtensionCredits, $egMapsServices, $egMapsMainServices, $wgParser, $wgExtensionCredits;
+// Add the services
+$egMapsServices['googlemaps']['qp'] = array('class' => 'SMGoogleMaps', 'file' => 'GoogleMaps/SM_GoogleMaps.php');
+$egMapsServices['googlemaps']['fi'] = array('class' => 'SMGoogleMapsFormInput', 'file' => 'GoogleMaps/SM_GoogleMapsFormInput.php');
 
-	foreach($egMapsMainServices as $service) smfInitFormat($service);
+$egMapsServices['yahoomaps']['qp'] = array('class' => 'SMYahooMaps', 'file' => 'YahooMaps/SM_YahooMaps.php');
+$egMapsServices['yahoomaps']['fi'] = array('class' => 'SMYahooMapsFormInput', 'file' => 'YahooMaps/SM_YahooMapsFormInput.php');
+
+$egMapsServices['openlayers']['qp'] = array('class' => 'SMOpenLayers', 'file' => 'OpenLayers/SM_OpenLayers.php');
+$egMapsServices['openlayers']['fi'] = array('class' => 'SMOpenLayersFormInput', 'file' => 'OpenLayers/SM_OpenLayersFormInput.php');
+
+function smfSetup() {
+	global $wgExtensionCredits, $egMapsMainServices, $egMapsServices;
 
 	$services_list = implode(', ', $egMapsMainServices);
 	
@@ -46,47 +54,38 @@ function smfSetup() {
 		'author' => array("[http://bn2vs.com Jeroen De Dauw]", "Yaron Koren", "Robert Buzink"),
 		'url' => 'http://www.mediawiki.org/wiki/Extension:Semantic_Maps',
 		'description' => wfMsg( 'semanticmaps_desc', $services_list ),
-		'descriptionmsg' => array( 'semanticmaps_desc', $services_list ),
+		'descriptionmsg' => wfMsg( 'semanticmaps_desc', $services_list ),
 	);
 
-	smfInitializeService('map', 'SMMapper');
+	smfInitFormHook('map');
+	smfInitFormat('map', array('class' => 'SMMapper', 'file' => 'SM_Mapper.php'));
 	
-	smfInitializeServiceAliases('googlemaps', 'SMGoogleMaps');
-	smfInitializeServiceAliases('yahoomaps', 'SMYahooMaps');
-	smfInitializeServiceAliases('openlayers', 'SMOpenLayers');
+	foreach($egMapsServices as $serviceName => $serviceData) {
+		$hasQP = array_key_exists('qp', $serviceData);
+		$hasFI = array_key_exists('fi', $serviceData);
+		
+		if ($hasQP) smfInitFormat($serviceName, $serviceData['qp']);
+		if ($hasFI) smfInitFormHook($serviceName, $serviceData['fi']);
+		
+		foreach ($serviceData['aliases'] as $alias) {
+			if ($hasQP) smfInitFormat($alias, $serviceData['qp']);
+			if ($hasFI) smfInitFormHook($alias, $serviceData['fi'], $serviceName);
+		}
+	}
+	
 }
 
 /**
- * Apply smfInitializeService() to a service and all it's aliases.
- *
- * @param unknown_type $mainServiceName
- * @param unknown_type $queryPrinter
- */
-function smfInitializeServiceAliases($mainServiceName, $queryPrinter) {
-	global $egMapsServices;
-	
-	smfInitializeService($mainServiceName, $queryPrinter, $mainServiceName);
-	foreach ($egMapsServices[$mainServiceName] as $alias) smfInitializeService($alias, $queryPrinter, $mainServiceName);
-}
-
-/**
- * Add the service name as result format for the provided query printer, 
- * and set a hook for it's form input.
+ * Adds a mapping service's form hook
  *
  * @param unknown_type $service
- * @param unknown_type $queryPrinter
  * @param unknown_type $mainName
  */
-function smfInitializeService($service, $queryPrinter, $mainName = '') {
-	global $smwgResultFormats, $sfgFormPrinter;
-	
-	// Check if $smwgResultFormats, a global variable introduced in SMW 1.2.2, is set
-	// and add the query printer to the result format.
-	if (isset($smwgResultFormats)) {
-		$smwgResultFormats[$service] = $queryPrinter;
-	}
-	else {
-		SMWQueryProcessor::$formats[$service] = $queryPrinter;
+function smfInitFormHook($service, $fi = null, $mainName = '') {
+	global $wgAutoloadClasses, $sfgFormPrinter, $smgIP;
+
+	if (isset($fi)) {
+		if (! array_key_exists($fi['class'], $wgAutoloadClasses)) $wgAutoloadClasses[$fi['class']] = $smgIP . '/' . $fi['file'];
 	}
 	
 	// Add the form input hook for the service
@@ -96,32 +95,22 @@ function smfInitializeService($service, $queryPrinter, $mainName = '') {
 }
 
 /**
- * Initialize the result format depending on the map service
+ * Add the result format for a mapping service or alias
+ *
+ * @param unknown_type $format
+ * @param unknown_type $qp
  */
-function smfInitFormat( $format ) {
-	global $smwgResultFormats, $wgAutoloadClasses, $smgIP;
-
-	switch ($format) {
-		case 'googlemaps':
-			$class = 'SMGoogleMaps';
-			$file = $smgIP . '/GoogleMaps/SM_GoogleMaps';
-		break;
-		case 'openlayers':
-			$class = 'SMOpenLayers';
-			$file = $smgIP . '/OpenLayers/SM_OpenLayers';
-		break;
-		case 'yahoomaps':
-			$class = 'SMYahooMaps';
-			$file = $smgIP . '/YahooMaps/SM_YahooMaps';
-		break;
+function smfInitFormat($format, $qp) {
+	global $wgAutoloadClasses, $smwgResultFormats, $smgIP;
+	
+	if (! array_key_exists($qp['class'], $wgAutoloadClasses)) $wgAutoloadClasses[$qp['class']] = $smgIP . '/' . $qp['file'];
+	
+	if (isset($smwgResultFormats)) {
+		$smwgResultFormats[$format] = $qp['class'];
 	}
-
-	if (isset($class) && isset($file)) {
-		$smwgResultFormats[$format] = $class;
-		$wgAutoloadClasses[$class] = $file . ".php";
-		$wgAutoloadClasses[$class . "FormInput"] = $file . "FormInput.php";
-	}
-
+	else {
+		SMWQueryProcessor::$formats[$format] = $qp['class'];
+	}	
 }
 
 /**
@@ -135,14 +124,13 @@ function smfInitFormat( $format ) {
  * @return unknown
  */
 function smfSelectFormInputHTML($coordinates, $input_name, $is_mandatory, $is_disabled, $field_args) {
-	global $egMapsAvailableServices, $egMapsDefaultService;
-	
 	// If service_name is set, use this value, and ignore any given service parameters
 	// This will prevent ..input type=googlemaps|service=yahoo.. from shwoing up a Yahoo! Maps map
 	if (array_key_exists('service_name', $field_args)) $field_args['service'] = $field_args['service_name'];
 		
 	$field_args['service'] = MapsMapper::getValidService($field_args['service']);
 	
+	// TODO: get class from hook system afetr check if fi is available
 	// Create an instace of 
 	switch ($field_args['service']) {
 		case 'googlemaps' :
