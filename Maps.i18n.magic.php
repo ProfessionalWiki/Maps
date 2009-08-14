9d18d619039c6ecf158fedca51f3f0d1778c05b1
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-14 14:19:47 +0000

    Localisation updates for extension magic words from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@55018 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.i18n.magic.php
+++ Maps.i18n.magic.php
@@ -11,3 +11,13 @@ $magicWords['en'] = array(
 	'geocodelat'        => array( 0, 'geocodelat' ),
 	'geocodelng'        => array( 0, 'geocodelng' ),
 );
+
+$magicWords['nl'] = array(
+	'display_point'       => array( '0', 'punt_weergeven', 'display_point' ),
+	'display_points'      => array( '0', 'punten_weergeven', 'display_points' ),
+	'display_address'     => array( '0', 'adres_weergeven', 'display_address' ),
+	'display_addresses'   => array( '0', 'adressen_weergeven', 'display_addresses' ),
+	'geocode'             => array( '0', 'geocoderen', 'geocode' ),
+	'geocodelat'          => array( '0', 'breedtegeocoderen', 'geocodelat' ),
+	'geocodelng'          => array( '0', 'lengtegeocoderen', 'geocodelng' ),
+);
