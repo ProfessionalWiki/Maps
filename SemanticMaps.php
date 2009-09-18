72687b79708ed66ece6582ce85bf3010f33c8b88
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-18 18:52:59 +0000

    Changes for 0.3.5
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticMaps@56615 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticMaps.php
+++ SemanticMaps.php
@@ -23,7 +23,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define('SM_VERSION', '0.3.4');
+define('SM_VERSION', '0.3.5');
 
 $smgScriptPath 	= $wgScriptPath . '/extensions/SemanticMaps';
 $smgIP 			= $IP . '/extensions/SemanticMaps';
@@ -100,13 +100,15 @@ function smfGetDynamicInput($id, $value, $args='') {
  */
 function smfAddToAdminLinks(&$admin_links_tree) {
     $displaying_data_section = $admin_links_tree->getSection(wfMsg('smw_adminlinks_displayingdata'));
+    
     // Escape if SMW hasn't added links
-    if (is_null($displaying_data_section))
-        return true;
+    if (is_null($displaying_data_section)) return true;
+    
     $smw_docu_row = $displaying_data_section->getRow('smw');
-    wfLoadExtensionMessages('SemanticMaps');
+    
     $sm_docu_label = wfMsg('adminlinks_documentation', wfMsg('semanticmaps_name'));
     $smw_docu_row->addItem(AlItem::newFromExternalLink("http://www.mediawiki.org/wiki/Extension:Semantic_Maps", $sm_docu_label));
+    
     return true;
 }
 
