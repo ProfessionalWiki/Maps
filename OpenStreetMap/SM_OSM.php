072bbad2ddc8741df8603a5bfea33368ab418f69
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-09 20:20:18 +0000

    Changes for 0.4.1 (a2). http://www.mediawiki.org/wiki/Extension:Semantic_Maps/Future#Semantic_Maps_0.4.1
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticMaps@58832 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ OpenStreetMap/SM_OSM.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This groupe contains all OpenStreetMap related files of the Semantic Maps extension.
+ * 
+ * @defgroup SMOpenStreetMap OpenStreetMap
+ * @ingroup SemanticMaps
+ */
+
+/**
+ * This file holds the general information for the OpenStreetMap service.
+ *
+ * @file SM_OpenStreetMap.php
+ * @ingroup SMOpenStreetMap
+ *
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+// $egMapsServices['osm']['qp'] = array('class' => 'SMOSMQP', 'file' => 'SemanticMaps/OpenStreetMap/SM_OSMQP.php', 'local' => false);
+// $egMapsServices['osm']['fi'] = array('class' => 'SMOSMFormInput', 'file' => 'SemanticMaps/OpenStreetMap/SM_OSMFormInput.php', 'local' => false);
