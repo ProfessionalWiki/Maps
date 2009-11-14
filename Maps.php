e29fa45e7e45a842264ac3dcbb8483eabb46c7e5
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-14 16:33:08 +0000

    Changes for 0.4.2 (rc2) (see http://www.mediawiki.org/wiki/Extension:Maps/Version_history)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@59066 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.php
+++ Maps.php
@@ -23,7 +23,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define('Maps_VERSION', '0.4.2 rc1');
+define('Maps_VERSION', '0.4.2 rc2');
 
 $egMapsScriptPath 	= $wgScriptPath . '/extensions/Maps';
 $egMapsIP 			= $IP . '/extensions/Maps';
