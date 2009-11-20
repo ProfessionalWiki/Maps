9be7b9e457441e6a0cf19ebf90e291abef9e1a0a
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-20 17:04:35 +0000

    Changes for 0.5. http://www.mediawiki.org/wiki/Extension:Semantic_Maps/Future#Semantic_Maps_0.5
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticMaps@59291 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticMaps.php
+++ SemanticMaps.php
@@ -23,7 +23,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define('SM_VERSION', '0.5 a1');
+define('SM_VERSION', '0.5 a3');
 
 $smgScriptPath 	= $wgScriptPath . '/extensions/SemanticMaps';
 $smgIP 			= $IP . '/extensions/SemanticMaps';
