77d4ce9c44de2b71a4c2fd36d9672abcab1c6557
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-29 21:38:29 +0000

    Added parameter-description messages, for use by getParameters() in query printers
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticMaps@59569 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticMaps.i18n.php
+++ SemanticMaps.i18n.php
@@ -22,6 +22,18 @@ Available map services: $1",
 	'semanticmaps_lookupcoordinates' => 'Look up coordinates',
 	'semanticmaps_enteraddresshere' => 'Enter address here',
 	'semanticmaps_notfound' => 'not found',
+	'semanticmaps_paramdesc_format' => 'The mapping service used to generate the map',
+	'semanticmaps_paramdesc_geoservice' => 'The geocoding service used to turn addresses into coordinates',
+	'semanticmaps_paramdesc_height' => 'The height of the map, in pixels (default is $1)',
+	'semanticmaps_paramdesc_width' => 'The width of the map, in pixels (default is $1)',
+	'semanticmaps_paramdesc_zoom' => 'The zoom level of the map',
+	'semanticmaps_paramdesc_centre' => 'The coordinates of the maps\' centre',
+	'semanticmaps_paramdesc_controls' => 'The user controls placed on the map',
+	'semanticmaps_paramdesc_types' => 'The map types available on the map',
+	'semanticmaps_paramdesc_type' => 'The default map type for the map',
+	'semanticmaps_paramdesc_overlays' => 'The overlays available on the map',
+	'semanticmaps_paramdesc_autozoom' => 'Whether or not one can zoom in and out by using the mouse scroll wheel',
+	'semanticmaps_paramdesc_layers' => 'The layers available on the map',
 );
 
 /** Message documentation (Message documentation)
