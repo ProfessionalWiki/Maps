1a0efecea284a7efa6c0eb57d6a994d06394d044
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-23 15:09:23 +0000

    Changes for 0.5.3. * Google Maps v3 support
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@61416 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- GoogleMaps3/GoogleMap3Functions.js
+++ GoogleMaps3/GoogleMap3Functions.js
@@ -17,8 +17,25 @@ function initGMap3(name, options, markerData) {
 	
 	// TODO: types - http://code.google.com/apis/maps/documentation/v3/reference.html#MapTypeRegistry
 	
-	// TODO: markers
+	for (marker in markerData) getGMaps3Marker(map, markerData[marker]);
 }
+
+function getGMaps3Marker(map, data) {
+	var marker = new google.maps.Marker({position: data.position, map: map, title: data.title});
+	
+	var bothTxtAreSet = data.title.length > 0 && data.label.length > 0;
+	var popupText = bothTxtAreSet ? '<b>' + data.title + '</b><hr />' + data.label : data.title + data.label;	
+	
+	var infowindow = new google.maps.InfoWindow({content: popupText});
+	
+	google.maps.event.addListener(marker, "click", function() {
+		infowindow.close();
+        infowindow.open(map, marker);    		
+	});
+	
+	return marker;
+}
+
 
 function getGMaps3MarkerData(lat, lon, title, label, icon) {
 	return {position: new google.maps.LatLng(lat, lon), title: title, label: label, icon: icon};
