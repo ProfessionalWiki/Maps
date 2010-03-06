34cc77eea18ce0e661d7bbdeb6266f8351e7fb3e
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-06 18:08:41 +0000

    Small tweak to google maps popup scrollbar
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@63327 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- GoogleMaps/GoogleMapFunctions.js
+++ GoogleMaps/GoogleMapFunctions.js
@@ -35,7 +35,7 @@ function createGMarker(point, title, label, icon) {
 	if ((title + label).length > 0) {
 		var bothTxtAreSet = title.length > 0 && label.length > 0;
 		var popupText = bothTxtAreSet ? '<b>' + title + '</b><hr />' + label : title + label;	
-		popupText = '<div style="overflow:auto;max-height:150px;">' + popupText + '</div>';
+		popupText = '<div style="overflow:auto;max-height:140px;">' + popupText + '</div>';
 
 		GEvent.addListener(marker, 'click',
 			function() {
