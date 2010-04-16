de318a3d8c538ecf1873c9c52ef99cacd4feecde
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-16 20:41:14 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-04-16 20:20:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@65138 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.i18n.magic.php
+++ Maps.i18n.magic.php
@@ -14,21 +14,17 @@ $magicWords['en'] = array(
 $magicWords['ar'] = array(
 	'display_point'       => array( '0', 'نقطة_العرض', 'display_point' ),
 	'display_points'      => array( '0', 'نقاط_العرض', 'display_points' ),
-	'display_address'     => array( '0', 'عنوان_العرض', 'display_address' ),
-	'display_addresses'   => array( '0', 'عناوين_العرض', 'display_addresses' ),
 	'geocode'             => array( '0', 'كود_جغرافي', 'geocode' ),
 	'geocodelat'          => array( '0', 'كود_جغرافي_طول', 'geocodelat' ),
-	'geocodelng'          => array( '0', 'كود_جغرافي_عرض', 'geocodelng' ),
+	'geocodelon'          => array( '0', 'كود_جغرافي_عرض', 'geocodelon' ),
 );
 
 $magicWords['arz'] = array(
 	'display_point'       => array( '0', 'نقطة_العرض', 'display_point' ),
 	'display_points'      => array( '0', 'نقاط_العرض', 'display_points' ),
-	'display_address'     => array( '0', 'عنوان_العرض', 'display_address' ),
-	'display_addresses'   => array( '0', 'عناوين_العرض', 'display_addresses' ),
 	'geocode'             => array( '0', 'كود_جغرافي', 'geocode' ),
 	'geocodelat'          => array( '0', 'كود_جغرافى_طول', 'كود_جغرافي_طول', 'geocodelat' ),
-	'geocodelng'          => array( '0', 'كود_جغرافى_عرض', 'كود_جغرافي_عرض', 'geocodelng' ),
+	'geocodelon'          => array( '0', 'كود_جغرافى_عرض', 'كود_جغرافي_عرض', 'geocodelon' ),
 );
 
 $magicWords['es'] = array(
@@ -38,30 +34,21 @@ $magicWords['es'] = array(
 	'display_addresses'   => array( '0', 'mostrar_direcciones', 'display_addresses' ),
 	'geocode'             => array( '0', 'geocodificar', 'geocode' ),
 	'geocodelat'          => array( '0', 'geocodificarlat', 'geocodelat' ),
-	'geocodelng'          => array( '0', 'geocodificarlng', 'geocodificarlong', 'geocodelng' ),
-);
-
-$magicWords['mr'] = array(
-	'display_address'     => array( '0', 'पत्ता_दाखवा', 'display_address' ),
-	'display_addresses'   => array( '0', 'पत्ते दाखवा', 'display_addresses' ),
+	'geocodelon'          => array( '0', 'geocodificarlng', 'geocodificarlong', 'geocodelon' ),
 );
 
 $magicWords['nl'] = array(
 	'display_point'       => array( '0', 'punt_weergeven', 'display_point' ),
 	'display_points'      => array( '0', 'punten_weergeven', 'display_points' ),
-	'display_address'     => array( '0', 'adres_weergeven', 'display_address' ),
-	'display_addresses'   => array( '0', 'adressen_weergeven', 'display_addresses' ),
 	'geocode'             => array( '0', 'geocoderen', 'geocode' ),
 	'geocodelat'          => array( '0', 'breedtegeocoderen', 'geocodelat' ),
-	'geocodelng'          => array( '0', 'lengtegeocoderen', 'geocodelng' ),
+	'geocodelon'          => array( '0', 'lengtegeocoderen', 'geocodelon' ),
 );
 
 $magicWords['ru'] = array(
 	'display_point'       => array( '0', 'показать_точку', 'display_point' ),
 	'display_points'      => array( '0', 'показать_точки', 'display_points' ),
-	'display_address'     => array( '0', 'показать_адрес', 'display_address' ),
-	'display_addresses'   => array( '0', 'показать_адреса', 'display_addresses' ),
 	'geocode'             => array( '0', 'геокод', 'geocode' ),
 	'geocodelat'          => array( '0', 'геокодшир', 'geocodelat' ),
-	'geocodelng'          => array( '0', 'геокоддол', 'geocodelng' ),
+	'geocodelon'          => array( '0', 'геокоддол', 'geocodelon' ),
 );
