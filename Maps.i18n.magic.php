e5e55758aca87e3545313d6d76cbf26c041054df
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-28 17:24:32 +0000

    i18n update
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@65618 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.i18n.magic.php
+++ Maps.i18n.magic.php
@@ -23,34 +23,31 @@ $magicWords['ar'] = array(
 );
 
 $magicWords['arz'] = array(
-	'display_point'       => array( '0', 'نقطة_العرض', 'display_point' ),
-	'display_points'      => array( '0', 'نقاط_العرض', 'display_points' ),
+	'display_point'       => array( '0', 'نقطة_العرض', 'display_point', 'display_points' ),
 	'geocode'             => array( '0', 'كود_جغرافي', 'geocode' ),
 	'geocodelat'          => array( '0', 'كود_جغرافى_طول', 'كود_جغرافي_طول', 'geocodelat' ),
 	'geocodelon'          => array( '0', 'كود_جغرافى_عرض', 'كود_جغرافي_عرض', 'geocodelon' ),
 );
 
 $magicWords['es'] = array(
-	'display_point'       => array( '0', 'mostrar_punto', 'display_point' ),
-	'display_points'      => array( '0', 'mostrar_puntos', 'display_points' ),
-	'display_address'     => array( '0', 'mostrar_dirección', 'mostrar_direccion', 'display_address' ),
-	'display_addresses'   => array( '0', 'mostrar_direcciones', 'display_addresses' ),
+	'display_point'       => array( '0', 'mostrar_punto', 'display_point', 'display_points' ),
 	'geocode'             => array( '0', 'geocodificar', 'geocode' ),
 	'geocodelat'          => array( '0', 'geocodificarlat', 'geocodelat' ),
 	'geocodelon'          => array( '0', 'geocodificarlng', 'geocodificarlong', 'geocodelon' ),
 );
 
 $magicWords['nl'] = array(
-	'display_point'       => array( '0', 'punt_weergeven', 'display_point' ),
-	'display_points'      => array( '0', 'punten_weergeven', 'display_points' ),
+	'display_map'         => array( '0', 'map_weergeven', 'display_map' ),
+	'display_point'       => array( '0', 'punt_weergeven', 'display_point', 'display_points' ),
 	'geocode'             => array( '0', 'geocoderen', 'geocode' ),
 	'geocodelat'          => array( '0', 'breedtegeocoderen', 'geocodelat' ),
 	'geocodelon'          => array( '0', 'lengtegeocoderen', 'geocodelon' ),
+	'geodistance'         => array( '0', 'geoafstand', 'geodistance' ),
+	'finddestination'     => array( '0', 'vindbestemming', 'finddestination' ),
 );
 
 $magicWords['ru'] = array(
-	'display_point'       => array( '0', 'показать_точку', 'display_point' ),
-	'display_points'      => array( '0', 'показать_точки', 'display_points' ),
+	'display_point'       => array( '0', 'показать_точку', 'display_point', 'display_points' ),
 	'geocode'             => array( '0', 'геокод', 'geocode' ),
 	'geocodelat'          => array( '0', 'геокодшир', 'geocodelat' ),
 	'geocodelon'          => array( '0', 'геокоддол', 'geocodelon' ),
