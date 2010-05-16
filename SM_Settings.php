38993f6d43827901792ed11d612eac16705ed5e4
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-16 02:51:52 +0000

    Improved docs
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticMaps@66507 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SM_Settings.php
+++ SM_Settings.php
@@ -21,68 +21,63 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 
 
 
-# Map features configuration
-# (named) Array of String. This array contains the available features for Maps.
-# The array element name contains an abbriviation, used for code references,
-# and in the service data arrays, the value is the human readible version for displaying purpouses.
-include_once $smgDir . 'Features/QueryPrinters/SM_QueryPrinters.php'; 	// Query printers
-include_once $smgDir . 'Features/FormInputs/SM_FormInputs.php'; 			// Form imputs							
+# Features configuration
 
-# Array of String. The default mapping service for each feature, which will be used when no valid service is provided by the user.
-# Each service needs to be enabled, if not, the first one from the available services will be taken.
-# Note: The default service needs to be available for the feature you set it for, since it's used as a fallback mechanism.
-$egMapsDefaultServices['qp'] = 'googlemaps2';
-$egMapsDefaultServices['fi'] = 'googlemaps2';
-
-
+	# (named) Array of String. This array contains the available features for Maps.
+	# Commenting out the inclusion of any feature will make Maps completely ignore it, and so improve performance.
+	
+		# Query printers
+		include_once $smgDir . 'Features/QueryPrinters/SM_QueryPrinters.php';
+		# Form imputs
+		include_once $smgDir . 'Features/FormInputs/SM_FormInputs.php'; 
 
 
 
 # Mapping services configuration
 
-# Include the mapping services that should be loaded into Semantic Maps. 
-# Commenting or removing a mapping service will cause Semantic Maps to completely ignore it, and so improve performance.
-include_once $smgDir . 'Services/GoogleMaps/SM_GoogleMaps.php'; 	// Google Maps
-include_once $smgDir . 'Services/OpenLayers/SM_OpenLayers.php'; 	// OpenLayers
-include_once $smgDir . 'Services/YahooMaps/SM_YahooMaps.php'; 	// Yahoo! Maps
-// include_once $smgDir . 'Services/OpenStreetMap/SM_OSM.php'; 		// OpenLayers optimized for OSM
+	# Include the mapping services that should be loaded into Semantic Maps. 
+	# Commenting or removing a mapping service will cause Semantic Maps to completely ignore it, and so improve performance.
+	# Google Maps API v2
+	include_once $smgDir . 'Services/GoogleMaps/SM_GoogleMaps.php';
+	# OpenLayers API
+	include_once $smgDir . 'Services/OpenLayers/SM_OpenLayers.php';
+	# Yahoo! Maps API
+	include_once $smgDir . 'Services/YahooMaps/SM_YahooMaps.php';
 
-
-
-
+	# Array of String. The default mapping service for each feature, which will be used when no valid service is provided by the user.
+	# Each service needs to be enabled, if not, the first one from the available services will be taken.
+	# Note: The default service needs to be available for the feature you set it for, since it's used as a fallback mechanism.
+	$egMapsDefaultServices['qp'] = 'googlemaps2';
+	$egMapsDefaultServices['fi'] = 'googlemaps2';
+	
+	
 
 # Queries
 
-# Boolean. The default value for the forceshow parameter. Will force a map to be shown even when there are no query results
-# when set to true. This value will only be used when the user does not provide one.
-$smgQPForceShow = false;
-
-# Boolean. The default value for the showtitle parameter. Will hide the title in the marker pop-ups when set to true. 
-# This value will only be used when the user does not provide one.
-$smgQPShowTitle = true;
-
-# String or false. Allows you to define the content and it's layout of marker pop-ups via a template.
-# This value will only be used when the user does not provide one.
-$smgQPTemplate = false;
-
-# Enum. The default output format of coordinates.
-# Possible values: Maps_COORDS_FLOAT, Maps_COORDS_DMS, Maps_COORDS_DM, Maps_COORDS_DD
-$smgQPCoodFormat = $egMapsCoordinateNotation;
-
-# Boolean. Indicates if coordinates should be outputted in directional notation by default.
-$smgQPCoodDirectional = $egMapsCoordinateDirectional;
-
-# Integer. The default distance value to be used in geographical distance queries.
-# This value will only be used when the user does not provide one.
-$smgGeoCoordDistance = 5;
-
-
+	# Boolean. The default value for the forceshow parameter. Will force a map to be shown even when there are no query results
+	# when set to true. This value will only be used when the user does not provide one.
+	$smgQPForceShow = false;
+	
+	# Boolean. The default value for the showtitle parameter. Will hide the title in the marker pop-ups when set to true. 
+	# This value will only be used when the user does not provide one.
+	$smgQPShowTitle = true;
+	
+	# String or false. Allows you to define the content and it's layout of marker pop-ups via a template.
+	# This value will only be used when the user does not provide one.
+	$smgQPTemplate = false;
+	
+	# Enum. The default output format of coordinates.
+	# Possible values: Maps_COORDS_FLOAT, Maps_COORDS_DMS, Maps_COORDS_DM, Maps_COORDS_DD
+	$smgQPCoodFormat = $egMapsCoordinateNotation;
+	
+	# Boolean. Indicates if coordinates should be outputted in directional notation by default.
+	$smgQPCoodDirectional = $egMapsCoordinateDirectional;
 
 
 
 # Forms
 
-# Integer or string. The default width and height of maps in forms created by using Semantic Forms.
-# These values only be used when the user does not provide them.
-$smgFIWidth = 665;
-$smgFIHeight = $egMapsMapHeight;
+	# Integer or string. The default width and height of maps in forms created by using Semantic Forms.
+	# These values only be used when the user does not provide them.
+	$smgFIWidth = 665;
+	$smgFIHeight = $egMapsMapHeight;
