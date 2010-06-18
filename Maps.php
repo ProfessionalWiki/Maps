feebf0ebc0bbaf76781a65fb22422e06b8e4bb5c
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-18 17:32:56 +0000

    removed unused globals
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@68223 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.php
+++ Maps.php
@@ -78,9 +78,9 @@ else {
  * Initialization function for the Maps extension.
  */
 function efMapsSetup() {
-	global $wgExtensionCredits, $wgLang, $wgAutoloadClasses, $IP, $wgStyleVersion, $wgScriptPath,  $wgJsMimeType, $wgExtensionAssetsPath;
-	global $egMapsDefaultService, $egMapsAvailableServices, $egMapsServices, $egMapsDefaultGeoService, $egMapsScriptPath;
-	global $egMapsDir, $egMapsAvailableFeatures, $egMapsUseMinJs, $egMapsJsExt, $egMapsStyleVersion;
+	global $wgExtensionCredits, $wgLang, $wgAutoloadClasses;
+	global $egMapsDefaultService, $egMapsAvailableServices, $egMapsServices;
+	global $egMapsDir, $egMapsUseMinJs, $egMapsJsExt;
 
 	// Autoload the includes/ classes.
 	$wgAutoloadClasses['MapsMapper'] 				= $egMapsDir . 'Includes/Maps_Mapper.php';
@@ -97,7 +97,7 @@ function efMapsSetup() {
 	require_once $egMapsDir . 'Services/Maps_iMappingService.php';
 	$wgAutoloadClasses['MapsMappingService'] = $egMapsDir . 'Services/Maps_MappingService.php';
 	
-	wfRunHooks( 'MappingServiceLoad' );	
+	wfRunHooks( 'MappingServiceLoad' );
 	
 	wfRunHooks( 'MappingFeatureLoad' );
 
