7bf7045bbb139eaff10c1de1453402d5b951a0d6
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-13 11:06:24 +0000

    Follow up to r69287, svn on my laptop is ignoring this file for some reason o_O
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@69306 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Features/Maps_iMappingFeature.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * File holding interface iMappingFeature.
+ * 
+ * @file Maps_iMappingFeature.php
+ * @ingroup Maps
+ * 
+ * @author Jeroen De Dauw
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+/**
+ * Interface that should be implemented by all mapping features that want to use the.
+ * 
+ * @since 0.6.5
+ * 
+ * @author Jeroen De Dauw
+ */
+interface iMappingFeature {
+	/**
+	 * Adds the HTML specific to the mapping service to the output.
+	 * 
+	 * @since 0.6.5
+	 * 
+	 * @return string
+	 */
+	function addSpecificMapHTML();
+	
+	/**
+	 * Returns the specific parameters by first checking if they have been initialized yet,
+	 * doing to work if this is not the case, and then returning them.
+	 * 
+	 * @since 0.6.5
+	 * 
+	 * @return array
+	 */
+	function getSpecificParameterInfo();
+}
