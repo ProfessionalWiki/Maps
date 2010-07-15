eb3421c62084cd1055f790a6a5c32e1498900e1e
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-15 14:52:51 +0000

    Added unfinished unit tests
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@69384 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ test/MapsCoordinateParserTest.php
@@ -0,0 +1,172 @@
+<?php
+
+require_once 'PHPUnit\Framework\TestCase.php';
+
+/**
+ * MapsCoordinateParser test case.
+ * 
+ * @ingroup Maps
+ * @since 0.6.5
+ * @author Jeroen De Dauw
+ */
+class MapsCoordinateParserTest extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @var MapsCoordinateParser
+	 */
+	private $MapsCoordinateParser;
+	
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp() {
+		parent::setUp ();
+		
+		// TODO Auto-generated MapsCoordinateParserTest::setUp()
+		
+
+		$this->MapsCoordinateParser = new MapsCoordinateParser(/* parameters */);
+	
+	}
+	
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+		// TODO Auto-generated MapsCoordinateParserTest::tearDown()
+		
+
+		$this->MapsCoordinateParser = null;
+		
+		parent::tearDown ();
+	}
+	
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct() {
+		// TODO Auto-generated constructor
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::parseCoordinates()
+	 */
+	public function testParseCoordinates() {
+		// TODO Auto-generated MapsCoordinateParserTest::testParseCoordinates()
+		$this->markTestIncomplete ( "parseCoordinates test not implemented" );
+		
+		MapsCoordinateParser::parseCoordinates(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::getCoordinatesType()
+	 */
+	public function testGetCoordinatesType() {
+		// Floats
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '55.7557860 N, 37.6176330 W' ), Maps_COORDS_FLOAT );
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '55.7557860, -37.6176330' ), Maps_COORDS_FLOAT );
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '55 S, 37.6176330 W' ), Maps_COORDS_FLOAT );
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '-55, -37.6176330' ), Maps_COORDS_FLOAT );	
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '5.5S,37W ' ), Maps_COORDS_FLOAT );
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '-5.5,-37 ' ), Maps_COORDS_FLOAT );
+
+		// Decimal Degrees 
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '55.7557860° N, 37.6176330° W' ), Maps_COORDS_DD );
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '55.7557860°, -37.6176330°' ), Maps_COORDS_DD );
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '55° S, 37.6176330 ° W' ), Maps_COORDS_DD );
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '-55°, -37.6176330 °' ), Maps_COORDS_DD );	
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '5.5°S,37°W ' ), Maps_COORDS_DD );
+		$this->assertEquals( MapsCoordinateParser::getCoordinatesType( '-5.5°,-37° ' ), Maps_COORDS_DD );		
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::areCoordinates()
+	 */
+	public function testAreCoordinates() {
+		// TODO Auto-generated MapsCoordinateParserTest::testAreCoordinates()
+		$this->markTestIncomplete ( "areCoordinates test not implemented" );
+		
+		MapsCoordinateParser::areCoordinates(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::formatCoordinates()
+	 */
+	public function testFormatCoordinates() {
+		// TODO Auto-generated MapsCoordinateParserTest::testFormatCoordinates()
+		$this->markTestIncomplete ( "formatCoordinates test not implemented" );
+		
+		MapsCoordinateParser::formatCoordinates(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::formatToArray()
+	 */
+	public function testFormatToArray() {
+		// TODO Auto-generated MapsCoordinateParserTest::testFormatToArray()
+		$this->markTestIncomplete ( "formatToArray test not implemented" );
+		
+		MapsCoordinateParser::formatToArray(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::areFloatCoordinates()
+	 */
+	public function testAreFloatCoordinates() {
+		// TODO Auto-generated MapsCoordinateParserTest::testAreFloatCoordinates()
+		$this->markTestIncomplete ( "areFloatCoordinates test not implemented" );
+		
+		MapsCoordinateParser::areFloatCoordinates(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::areDMSCoordinates()
+	 */
+	public function testAreDMSCoordinates() {
+		// TODO Auto-generated MapsCoordinateParserTest::testAreDMSCoordinates()
+		$this->markTestIncomplete ( "areDMSCoordinates test not implemented" );
+		
+		MapsCoordinateParser::areDMSCoordinates(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::areDDCoordinates()
+	 */
+	public function testAreDDCoordinates() {
+		// TODO Auto-generated MapsCoordinateParserTest::testAreDDCoordinates()
+		$this->markTestIncomplete ( "areDDCoordinates test not implemented" );
+		
+		MapsCoordinateParser::areDDCoordinates(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::areDMCoordinates()
+	 */
+	public function testAreDMCoordinates() {
+		// TODO Auto-generated MapsCoordinateParserTest::testAreDMCoordinates()
+		$this->markTestIncomplete ( "areDMCoordinates test not implemented" );
+		
+		MapsCoordinateParser::areDMCoordinates(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsCoordinateParser::parseAndFormat()
+	 */
+	public function testParseAndFormat() {
+		// TODO Auto-generated MapsCoordinateParserTest::testParseAndFormat()
+		$this->markTestIncomplete ( "parseAndFormat test not implemented" );
+		
+		MapsCoordinateParser::parseAndFormat(/* parameters */);
+	
+	}
+
+}
+
---
+++ test/MapsDistanceParserTest.php
@@ -0,0 +1,129 @@
+<?php
+
+require_once 'PHPUnit\Framework\TestCase.php';
+
+/**
+ * MapsDistanceParser test case.
+ * 
+ * @ingroup Maps
+ * @since 0.6.5
+ * @author Jeroen De Dauw
+ */
+class MapsDistanceParserTest extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @var MapsDistanceParser
+	 */
+	private $MapsDistanceParser;
+	
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp() {
+		parent::setUp ();
+		
+		// TODO Auto-generated MapsDistanceParserTest::setUp()
+		
+
+		$this->MapsDistanceParser = new MapsDistanceParser(/* parameters */);
+	
+	}
+	
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+		// TODO Auto-generated MapsDistanceParserTest::tearDown()
+		
+
+		$this->MapsDistanceParser = null;
+		
+		parent::tearDown ();
+	}
+	
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct() {
+		// TODO Auto-generated constructor
+	}
+	
+	/**
+	 * Tests MapsDistanceParser::parseDistance()
+	 */
+	public function testParseDistance() {
+		// TODO Auto-generated MapsDistanceParserTest::testParseDistance()
+		$this->markTestIncomplete ( "parseDistance test not implemented" );
+		
+		MapsDistanceParser::parseDistance(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsDistanceParser::formatDistance()
+	 */
+	public function testFormatDistance() {
+		// TODO Auto-generated MapsDistanceParserTest::testFormatDistance()
+		$this->markTestIncomplete ( "formatDistance test not implemented" );
+		
+		MapsDistanceParser::formatDistance(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsDistanceParser::parseAndFormat()
+	 */
+	public function testParseAndFormat() {
+		// TODO Auto-generated MapsDistanceParserTest::testParseAndFormat()
+		$this->markTestIncomplete ( "parseAndFormat test not implemented" );
+		
+		MapsDistanceParser::parseAndFormat(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsDistanceParser::isDistance()
+	 */
+	public function testIsDistance() {
+		// TODO Auto-generated MapsDistanceParserTest::testIsDistance()
+		$this->markTestIncomplete ( "isDistance test not implemented" );
+		
+		MapsDistanceParser::isDistance(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsDistanceParser::getUnitRatio()
+	 */
+	public function testGetUnitRatio() {
+		// TODO Auto-generated MapsDistanceParserTest::testGetUnitRatio()
+		$this->markTestIncomplete ( "getUnitRatio test not implemented" );
+		
+		MapsDistanceParser::getUnitRatio(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsDistanceParser::getValidUnit()
+	 */
+	public function testGetValidUnit() {
+		// TODO Auto-generated MapsDistanceParserTest::testGetValidUnit()
+		$this->markTestIncomplete ( "getValidUnit test not implemented" );
+		
+		MapsDistanceParser::getValidUnit(/* parameters */);
+	
+	}
+	
+	/**
+	 * Tests MapsDistanceParser::getUnits()
+	 */
+	public function testGetUnits() {
+		// TODO Auto-generated MapsDistanceParserTest::testGetUnits()
+		$this->markTestIncomplete ( "getUnits test not implemented" );
+		
+		MapsDistanceParser::getUnits(/* parameters */);
+	
+	}
+
+}
+
