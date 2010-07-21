3c6fecc3690cb74617cb3ced09d7753809306c7f
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-21 10:15:25 +0000

    Version increment to 0.6.5 rc1
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@69664 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.php
+++ Maps.php
@@ -33,7 +33,7 @@ if ( ! defined( 'Validator_VERSION' ) ) {
 	echo '<b>Warning:</b> You need to have <a href="http://www.mediawiki.org/wiki/Extension:Validator">Validator</a> installed in order to use <a href="http://www.mediawiki.org/wiki/Extension:Maps">Maps</a>.';
 }
 else {
-	define( 'Maps_VERSION', '0.6.5 a6' );
+	define( 'Maps_VERSION', '0.6.5 rc1' );
 
 	// The different coordinate notations.
 	define( 'Maps_COORDS_FLOAT', 'float' );
