bf8c3daf0c1119f3f108b5eebbb675d4e28d757f
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-21 10:35:42 +0000

    Updated demo's link
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticMaps@69668 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticMaps.i18n.php
+++ SemanticMaps.i18n.php
@@ -18,7 +18,7 @@ $messages['en'] = array(
 	// General
 	'semanticmaps_name' => 'Semantic Maps',
 	// TODO: update demo link to the new wiki, once it has 0.6.x running.
-	'semanticmaps_desc' => "Provides the ability to view and edit coordinate data stored through the Semantic MediaWiki extension ([http://wiki.bn2vs.com/wiki/Semantic_Maps demo]).
+	'semanticmaps_desc' => "Provides the ability to view and edit coordinate data stored through the Semantic MediaWiki extension ([http://mapping.referata.com/wiki/Semantic_Maps_examples demo's]).
 Available map services: $1",
 	'semanticmaps-unrecognizeddistance' => 'The value $1 is not a valid distance.',
 
