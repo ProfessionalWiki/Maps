6b0271f503ee6cdd1bd94300cfa67eba9f58fbb9
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-24 13:20:07 +0000

    Localisation updates for extensions from translatewiki.net * update magic words.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@71553 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.i18n.magic.php
+++ Maps.i18n.magic.php
@@ -14,6 +14,11 @@ $magicWords['en'] = array(
 	'distance'      	=> array( 0, 'distance' ),
 );
 
+$magicWords['af'] = array(
+	'geocode'           => array( '0', 'geokode', 'geocode' ),
+	'coordinates'       => array( '0', 'koördinate', 'coordinates' ),
+);
+
 $magicWords['ar'] = array(
 	'display_map'       => array( '0', 'عرض_الخريطة', 'display_map' ),
 	'display_point'     => array( '0', 'نقطة_العرض', 'نقاط_العرض', 'display_point', 'display_points' ),
@@ -27,10 +32,21 @@ $magicWords['ar'] = array(
 );
 
 $magicWords['arz'] = array(
+	'display_map'       => array( '0', 'عرض_الخريطه', 'عرض_الخريطة', 'display_map' ),
 	'display_point'     => array( '0', 'نقطة_العرض', 'نقاط_العرض', 'display_point', 'display_points' ),
 	'geocode'           => array( '0', 'كود_جغرافي', 'geocode' ),
 	'geocodelat'        => array( '0', 'كود_جغرافى_طول', 'كود_جغرافي_طول', 'geocodelat' ),
 	'geocodelon'        => array( '0', 'كود_جغرافى_عرض', 'كود_جغرافي_عرض', 'geocodelon' ),
+	'geodistance'       => array( '0', 'مسافه_جغرافيه', 'مسافة_جغرافية', 'geodistance' ),
+	'finddestination'   => array( '0', 'إيجاد_الوجهه', 'إيجاد_الوجهة', 'finddestination' ),
+	'coordinates'       => array( '0', 'إحداثيات', 'coordinates' ),
+);
+
+$magicWords['ce'] = array(
+	'display_point'     => array( '0', 'гайта_тlадам', 'показать_точку', 'display_point', 'display_points' ),
+	'geocode'           => array( '0', 'геагишар', 'геокод', 'geocode' ),
+	'geocodelat'        => array( '0', 'геагишаршуьр', 'геокодшир', 'geocodelat' ),
+	'geocodelon'        => array( '0', 'геагишарйахал', 'геокоддол', 'geocodelon' ),
 );
 
 $magicWords['es'] = array(
@@ -38,6 +54,23 @@ $magicWords['es'] = array(
 	'geocode'           => array( '0', 'geocodificar', 'geocode' ),
 	'geocodelat'        => array( '0', 'geocodificarlat', 'geocodelat' ),
 	'geocodelon'        => array( '0', 'geocodificarlng', 'geocodificarlong', 'geocodelon' ),
+);
+
+$magicWords['ig'] = array(
+	'display_map'       => array( '0', 'zí_otúzọr', 'display_map' ),
+);
+
+$magicWords['lad'] = array(
+	'distance'          => array( '0', 'distancia', 'distance' ),
+);
+
+$magicWords['ml'] = array(
+	'display_map'       => array( '0', 'ഭൂപടം_പ്രദർശിപ്പിക്കുക', 'display_map' ),
+	'display_point'     => array( '0', 'ബിന്ദു_പ്രദർശിപ്പിക്കുക', 'ബിന്ദുക്കൾ_പ്രദർശിപ്പിക്കുക', 'display_point', 'display_points' ),
+	'geodistance'       => array( '0', 'ഭൗമദൂരം', 'geodistance' ),
+	'finddestination'   => array( '0', 'ലക്ഷ്യംതിരയുക', 'finddestination' ),
+	'coordinates'       => array( '0', 'നിർദ്ദേശാങ്കങ്ങൾ', 'coordinates' ),
+	'distance'          => array( '0', 'ദൂരം', 'distance' ),
 );
 
 $magicWords['nl'] = array(
@@ -49,12 +82,39 @@ $magicWords['nl'] = array(
 	'geodistance'       => array( '0', 'geoafstand', 'geodistance' ),
 	'finddestination'   => array( '0', 'bestemmingzoeken', 'finddestination' ),
 	'coordinates'       => array( '0', 'coordinaten', 'coordinates' ),
-	'distance'       	=> array( '0', 'afstand', 'distance' ),
+	'distance'          => array( '0', 'afstand', 'distance' ),
+);
+
+$magicWords['ps'] = array(
+	'distance'          => array( '0', 'واټن', 'distance' ),
+);
+
+$magicWords['pt'] = array(
+	'display_map'       => array( '0', 'mostrar_mapa', 'display_map' ),
+	'display_point'     => array( '0', 'mostrar_ponto', 'mostrar_pontos', 'display_point', 'display_points' ),
+	'geocode'           => array( '0', 'geocódigo', 'geocode' ),
+	'geocodelat'        => array( '0', 'geocódigolat', 'geocodelat' ),
+	'geocodelon'        => array( '0', 'geocódigolon', 'geocodelon' ),
+	'geodistance'       => array( '0', 'geodistância', 'geodistance' ),
+	'finddestination'   => array( '0', 'encontrardestino', 'finddestination' ),
+	'coordinates'       => array( '0', 'coordenadas', 'coordinates' ),
+	'distance'          => array( '0', 'distância', 'distance' ),
 );
 
 $magicWords['ru'] = array(
+	'display_map'       => array( '0', 'показать_карту', 'display_map' ),
 	'display_point'     => array( '0', 'показать_точку', 'display_point', 'display_points' ),
 	'geocode'           => array( '0', 'геокод', 'geocode' ),
 	'geocodelat'        => array( '0', 'геокодшир', 'geocodelat' ),
 	'geocodelon'        => array( '0', 'геокоддол', 'geocodelon' ),
+	'geodistance'       => array( '0', 'георасстояние', 'geodistance' ),
+	'finddestination'   => array( '0', 'найти_местоположение', 'finddestination' ),
+	'coordinates'       => array( '0', 'координаты', 'coordinates' ),
+	'distance'          => array( '0', 'расстояние', 'distance' ),
+);
+
+$magicWords['si'] = array(
+	'finddestination'   => array( '0', 'ගමනාන්තය_සෙවීම', 'finddestination' ),
+	'coordinates'       => array( '0', 'ඛණ්ඩාංක', 'coordinates' ),
+	'distance'          => array( '0', 'දුර', 'distance' ),
 );
