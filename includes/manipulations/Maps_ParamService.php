70a78b8d145ca0372e1b7dff748553b90ab30c48
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-09-25 02:45:48 +0000

    Follow up to r73702
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@73706 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ includes/manipulations/Maps_ParamService.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Parameter manipulation ensuring the service is valid and loading adittional parameter definitions.
+ * 
+ * @since 0.7
+ * 
+ * @file Maps_ParamService.php
+ * @ingroup Validator
+ * @ingroup ParameterManipulations
+ * 
+ * @author Jeroen De Dauw
+ */
+class MapsParamService extends ItemParameterManipulation {
+	
+	/**
+	 * The mapping feature. Needed to determine which services are available.
+	 * 
+	 * @since 0.7
+	 * 
+	 * @var string
+	 */
+	protected $feature;
+	
+	/**
+	 * Adittional parameter definitions to load.
+	 * 
+	 * @since 0.7
+	 * 
+	 * @var array of Parameter
+	 */
+	protected $serviceParams;
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @since 0.7
+	 */
+	public function __construct( $feature, array $serviceParams = array() ) {
+		parent::__construct();
+		
+		$this->feature = $feature;
+		$this->serviceParams = $serviceParams;
+	}	
+	
+	/**
+	 * @see ItemParameterManipulation::doManipulation
+	 * 
+	 * @since 0.7
+	 */	
+	public function doManipulation( &$value, array &$parameters ) {
+		// Make sure the service is valid.
+		$value = MapsMappingService::getValidServiceName( $value, $this->feature );
+		
+		// Add the service specific service parameters.
+		$parameters = array_merge( $parameters, $this->serviceParams );
+	}
+	
+}
