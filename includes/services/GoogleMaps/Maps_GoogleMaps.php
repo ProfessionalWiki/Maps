ProfessionalWiki/Maps

commit e5b56bf4286e6e1831537002e3079cf30253e289

Changes for 0.7 - Follow up to r73766
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@73767 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/services/GoogleMaps/Maps_GoogleMaps.php
@@ -45,7 +45,7 @@ class MapsGoogleMaps extends MapsMappingService {
 	protected function initParameterInfo( array &$params ) {
 		global $egMapsGoogleMapsType, $egMapsGoogleMapsTypes, $egMapsGoogleAutozoom, $egMapsGMapControls, $egMapsGMapOverlays;
 		
-		//$params['zoom']->addCriterion( new CriterionInRange( 0, 20 ) );
+		//$params['zoom']->addCriteria( new CriterionInRange( 0, 20 ) );
 		//$params['zoom']->setDefault( self::getDefaultZoom() );
 		
 		$params['controls'] = new ListParameter(
@@ -104,6 +104,7 @@ class MapsGoogleMaps extends MapsMappingService {
 			ListParameter::DEFAULT_DELIMITER,
 			Parameter::TYPE_STRING,
 			$egMapsGMapOverlays,
+			array(),
 			array(
 				new CriterionGoogleOverlay( self::$overlayData )
 			)

includes/services/OpenLayers/Maps_OpenLayers.php
@@ -43,7 +43,7 @@ class MapsOpenLayers extends MapsMappingService {
 	protected function initParameterInfo( array &$params ) {
 		global $egMapsOLLayers, $egMapsOLControls, $egMapsOpenLayersZoom;
 		
-		//$params['zoom']->addCriterion( new CriterionInRange( 0, 19 ) );
+		//$params['zoom']->addCriteria( new CriterionInRange( 0, 19 ) );
 		//$params['zoom']->setDefault( self::getDefaultZoom() );		
 		
 		$params['controls'] = new ListParameter(

includes/services/YahooMaps/Maps_YahooMaps.php
@@ -45,7 +45,7 @@ class MapsYahooMaps extends MapsMappingService {
 	protected function initParameterInfo( array &$params ) {
 		global $egMapsYahooAutozoom, $egMapsYahooMapsType, $egMapsYahooMapsTypes, $egMapsYahooMapsZoom, $egMapsYMapControls;
 		
-		//$params['zoom']->addCriterion( new CriterionInRange( 1, 13 ) );
+		//$params['zoom']->addCriteria( new CriterionInRange( 1, 13 ) );
 		//$params['zoom']->setDefault( self::getDefaultZoom() );		
 		
 		$params['controls'] = new ListParameter(

Maps.php
@@ -36,7 +36,7 @@ if ( ! defined( 'Validator_VERSION' ) ) {
 	echo '<b>Warning:</b> You need to have <a href="http://www.mediawiki.org/wiki/Extension:Validator">Validator</a> installed in order to use <a href="http://www.mediawiki.org/wiki/Extension:Maps">Maps</a>.';
 }
 else {
-	define( 'Maps_VERSION', '0.7 alpha-4' );
+	define( 'Maps_VERSION', '0.7 alpha-5' );
 
 	// The different coordinate notations.
 	define( 'Maps_COORDS_FLOAT', 'float' );

includes/Maps_CoordinateParser.php
@@ -13,9 +13,10 @@
  * regex to also accept dm and dd, which can give unexpected results in certain
  * usecases. The different seperator support could also be made nice.
  * 
+ * @since 0.6
+ * 
  * @file Maps_CoordinateParser.php
  * @ingroup Maps
- * @since 0.6
  * 
  * @author Jeroen De Dauw
  */

includes/Maps_DistanceParser.php
@@ -1,21 +1,9 @@
 <?php
 
 /**
- * File holding class MapsDistanceParser.
- *
- * @file Maps_DistanceParser.php
- * @ingroup Maps
- *
- * @author Jeroen De Dauw
- */
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'Not an entry point.' );
-}
-
-/**
  * Static class for distance validation and parsing. Internal representatations are in meters.
  * 
+ * @file Maps_DistanceParser.php
  * @ingroup Maps
  * 
  * @since 0.6

includes/features/Maps_BaseMap.php
@@ -79,10 +79,11 @@ abstract class MapsBaseMap {
 	 * mapping services, calling the specific methods and finally returning the resulting output.
 	 *
 	 * @param array $params
+	 * @param Parser $parser
 	 * 
 	 * @return html
 	 */
-	public final function getMapHtml( array $params ) {
+	public final function getMapHtml( array $params, Parser $parser ) {
 		$this->setMapProperties( $params );
 		
 		$this->setCentre();
@@ -91,9 +92,9 @@ abstract class MapsBaseMap {
 			$this->zoom = $this->service->getDefaultZoom();
 		}
 		
-		$this->addSpecificMapHTML();
+		$this->addSpecificMapHTML( $parser );
 		
-		$this->service->addDependencies( $this->parser );
+		$this->service->addDependencies( $parser );
 		
 		return $this->output;
 	}

includes/iMappingService.php
@@ -1,22 +1,12 @@
 <?php
 
 /**
- * File holding interface iMapParserFunction.
- * 
- * @file iMappingService.php
- * @ingroup Maps
- * 
- * @author Jeroen De Dauw
- */
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'Not an entry point.' );
-}
-
-/**
  * Interface that should be implemented by all mapping feature classes.
  * 
  * @since 0.6.3
+ * 
+ * @file iMappingService.php
+ * @ingroup Maps
  * 
  * @author Jeroen De Dauw
  */

commit 0b0b510a517dc805980e74455523fe1726944d77

Fixed some fails from r73767
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@73769 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/services/GoogleMaps/Maps_GoogleMaps.php
@@ -82,7 +82,7 @@ class MapsGoogleMaps extends MapsMappingService {
 				new CriterionInArray( array_keys( self::$mapTypes ) ),
 			)
 		);
-		$params['types']->addManipulations( new MapsParamGMapType() );		
+		$params['types']->addManipulations( new MapsParamGMapType(), new ParamManipulationImplode( ',', "'" ) );		
 		
 		$params['autozoom'] = new Parameter(
 			'autozoom',

includes/services/OpenLayers/Maps_OpenLayers.php
@@ -65,7 +65,7 @@ class MapsOpenLayers extends MapsMappingService {
 			$egMapsOLLayers,
 			array(),
 			array(
-				new CriterionInArray( self::getLayerNames( true ) ),
+				new CriterionInArray( self::getLayerNames( true ), new ParamManipulationImplode( ',', "'" ) ),
 			)			
 		);	
 

includes/services/YahooMaps/Maps_YahooMaps.php
@@ -68,7 +68,7 @@ class MapsYahooMaps extends MapsMappingService {
 			array(
 				new CriterionInArray( self::getTypeNames() ),
 			),
-			array( 'types' )		
+			array( 'types' )
 		);
 		$params['type']->addManipulations( new MapsParamYMapType() );
 
@@ -82,7 +82,7 @@ class MapsYahooMaps extends MapsMappingService {
 				new CriterionInArray( self::getTypeNames() ),
 			)
 		);
-		$params['type']->addManipulations( new MapsParamYMapType() );
+		$params['types']->addManipulations( new MapsParamYMapType(), new ParamManipulationImplode( ',', "'" ) );
 		
 		$params['autozoom'] = new Parameter(
 			'autozoom',