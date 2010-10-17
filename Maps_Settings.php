427bc8b85fe665f9e388c32bda78fd4b4d4aa4d7
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-17 04:48:44 +0000

    Changes for 0.7.1 - namespace number fix
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@74879 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps_Settings.php
+++ Maps_Settings.php
@@ -223,7 +223,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 # Other general configuration
 	
 	# Namespace index start of the mapping namespaces.
-	$egMapsNamespaceIndex = 9001;
+	$egMapsNamespaceIndex = 420;
 	
 	
 	
@@ -378,7 +378,6 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 			'osmarender',
 			'osm-cyclemap',
 			'osm-mapnik'
-		
 		);
 		
 		# The difinitions for the layers that should be available for the user.
@@ -397,19 +396,6 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 		
 			'nasa' => 'OpenLayers.Layer.WMS("NASA Global Mosaic", "http://t1.hypercube.telascience.org/cgi-bin/landsat7",
 				{layers: "landsat7", "sphericalMercator":true} )',
-		
-			/* FIXME: does not work properly yet
-			'wikipediaworld' => 'OpenLayers.Layer.Vector("Wikipedia World", {
-		strategies: [new OpenLayers.Strategy.BBOX( { ratio : 1.1, resFactor: 1 })],
-		protocol: new OpenLayers.Protocol.HTTP({
-				url: "http://toolserver.org/~kolossos/geoworld/marks.php?LANG=de",
-				format: new OpenLayers.Format.KML({
-                           extractStyles: true, 
-                           extractAttributes: true
-                })		
-        })
-	})'
-	*/
 		);
 		
 		# Layer group definitions. Group names must be different from layer names, and
