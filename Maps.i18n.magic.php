c7b08c90841e56c2bde49d4ed9277d1d1c1d1a4d
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@75726 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.i18n.magic.php
+++ Maps.i18n.magic.php
@@ -2,103 +2,141 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
-	'display_map'       => array( 0, 'display_map' ),
-	'display_point'     => array( 0, 'display_point', 'display_points' ),
-	'geocode'           => array( 0, 'geocode' ),
-	'geodistance'       => array( 0, 'geodistance' ),
-	'finddestination'   => array( 0, 'finddestination' ),
-	'coordinates'       => array( 0, 'coordinates' ),
-	'distance'      	=> array( 0, 'distance' ),
+	'display_map' => array( 0, 'display_map' ),
+	'display_point' => array( 0, 'display_point', 'display_points' ),
+	'geocode' => array( 0, 'geocode' ),
+	'geodistance' => array( 0, 'geodistance' ),
+	'finddestination' => array( 0, 'finddestination' ),
+	'coordinates' => array( 0, 'coordinates' ),
+	'distance' => array( 0, 'distance' ),
 );
 
+/** Afrikaans (Afrikaans) */
 $magicWords['af'] = array(
-	'geocode'           => array( '0', 'geokode', 'geocode' ),
-	'coordinates'       => array( '0', 'koördinate', 'coordinates' ),
+	'geocode' => array( 0, 'geokode', 'geocode' ),
+	'coordinates' => array( 0, 'koördinate', 'coordinates' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'display_map'       => array( '0', 'عرض_الخريطة', 'display_map' ),
-	'display_point'     => array( '0', 'نقطة_العرض', 'نقاط_العرض', 'display_point', 'display_points' ),
-	'geocode'           => array( '0', 'كود_جغرافي', 'geocode' ),
-	'geodistance'       => array( '0', 'مسافة_جغرافية', 'geodistance' ),
-	'finddestination'   => array( '0', 'إيجاد_الوجهة', 'finddestination' ),
-	'coordinates'       => array( '0', 'إحداثيات', 'coordinates' ),
-	'distance'          => array( '0', 'مسافة', 'distance' ),
+	'display_map' => array( 0, 'عرض_الخريطة', 'display_map' ),
+	'display_point' => array( 0, 'نقطة_العرض', 'نقاط_العرض', 'display_point', 'display_points' ),
+	'geocode' => array( 0, 'كود_جغرافي', 'geocode' ),
+	'geodistance' => array( 0, 'مسافة_جغرافية', 'geodistance' ),
+	'finddestination' => array( 0, 'إيجاد_الوجهة', 'finddestination' ),
+	'coordinates' => array( 0, 'إحداثيات', 'coordinates' ),
+	'distance' => array( 0, 'مسافة', 'distance' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'display_map'       => array( '0', 'عرض_الخريطه', 'عرض_الخريطة', 'display_map' ),
-	'display_point'     => array( '0', 'نقطة_العرض', 'نقاط_العرض', 'display_point', 'display_points' ),
-	'geocode'           => array( '0', 'كود_جغرافي', 'geocode' ),
-	'geodistance'       => array( '0', 'مسافه_جغرافيه', 'مسافة_جغرافية', 'geodistance' ),
-	'finddestination'   => array( '0', 'إيجاد_الوجهه', 'إيجاد_الوجهة', 'finddestination' ),
-	'coordinates'       => array( '0', 'إحداثيات', 'coordinates' ),
+	'display_map' => array( 0, 'عرض_الخريطه', 'عرض_الخريطة', 'display_map' ),
+	'display_point' => array( 0, 'نقطة_العرض', 'نقاط_العرض', 'display_point', 'display_points' ),
+	'geocode' => array( 0, 'كود_جغرافي', 'geocode' ),
+	'geodistance' => array( 0, 'مسافه_جغرافيه', 'مسافة_جغرافية', 'geodistance' ),
+	'finddestination' => array( 0, 'إيجاد_الوجهه', 'إيجاد_الوجهة', 'finddestination' ),
+	'coordinates' => array( 0, 'إحداثيات', 'coordinates' ),
 );
 
+/** Chechen (Нохчийн) */
 $magicWords['ce'] = array(
-	'display_point'     => array( '0', 'гайта_тlадам', 'показать_точку', 'display_point', 'display_points' ),
-	'geocode'           => array( '0', 'геагишар', 'геокод', 'geocode' ),
+	'display_point' => array( 0, 'гайта_тlадам', 'показать_точку', 'display_point', 'display_points' ),
+	'geocode' => array( 0, 'геагишар', 'геокод', 'geocode' ),
 );
 
+/** Spanish (Español) */
 $magicWords['es'] = array(
-	'display_point'     => array( '0', 'mostrar_punto', 'display_point', 'display_points' ),
-	'geocode'           => array( '0', 'geocodificar', 'geocode' ),
+	'display_point' => array( 0, 'mostrar_punto', 'display_point', 'display_points' ),
+	'geocode' => array( 0, 'geocodificar', 'geocode' ),
 );
 
+/** Igbo (Igbo) */
 $magicWords['ig'] = array(
-	'display_map'       => array( '0', 'zí_otúzọr', 'display_map' ),
+	'display_map' => array( 0, 'zí_otúzọr', 'display_map' ),
 );
 
+/** Italian (Italiano) */
+$magicWords['it'] = array(
+	'display_map' => array( 0, 'mostra_mappa' ),
+	'display_point' => array( 0, 'mostra_punto', 'mostra_punti' ),
+	'coordinates' => array( 0, 'coordinate' ),
+	'distance' => array( 0, 'distanza' ),
+);
+
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'display_map' => array( 0, '地図表示' ),
+	'display_point' => array( 0, '地図点' ),
+	'geocode' => array( 0, '地理的コード' ),
+	'geodistance' => array( 0, '地理的距離' ),
+	'finddestination' => array( 0, '目的地検索' ),
+	'coordinates' => array( 0, '座標' ),
+	'distance' => array( 0, '距離' ),
+);
+
+/** Ladino (Ladino) */
 $magicWords['lad'] = array(
-	'distance'          => array( '0', 'distancia', 'distance' ),
+	'distance' => array( 0, 'distancia', 'distance' ),
 );
 
+/** Malayalam (മലയാളം) */
 $magicWords['ml'] = array(
-	'display_map'       => array( '0', 'ഭൂപടം_പ്രദർശിപ്പിക്കുക', 'display_map' ),
-	'display_point'     => array( '0', 'ബിന്ദു_പ്രദർശിപ്പിക്കുക', 'ബിന്ദുക്കൾ_പ്രദർശിപ്പിക്കുക', 'display_point', 'display_points' ),
-	'geodistance'       => array( '0', 'ഭൗമദൂരം', 'geodistance' ),
-	'finddestination'   => array( '0', 'ലക്ഷ്യംതിരയുക', 'finddestination' ),
-	'coordinates'       => array( '0', 'നിർദ്ദേശാങ്കങ്ങൾ', 'coordinates' ),
-	'distance'          => array( '0', 'ദൂരം', 'distance' ),
+	'display_map' => array( 0, 'ഭൂപടം_പ്രദർശിപ്പിക്കുക' ),
+	'display_point' => array( 0, 'ബിന്ദു_പ്രദർശിപ്പിക്കുക', 'ബിന്ദുക്കൾ_പ്രദർശിപ്പിക്കുക' ),
+	'geodistance' => array( 0, 'ഭൗമദൂരം' ),
+	'finddestination' => array( 0, 'ലക്ഷ്യംതിരയുക' ),
+	'coordinates' => array( 0, 'നിർദ്ദേശാങ്കങ്ങൾ' ),
+	'distance' => array( 0, 'ദൂരം' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'display_map'       => array( '0', 'kaart_weergeven', 'display_map' ),
-	'display_point'     => array( '0', 'punt_weergeven', 'display_point', 'display_points' ),
-	'geocode'           => array( '0', 'geocoderen', 'geocode' ),
-	'geodistance'       => array( '0', 'geoafstand', 'geodistance' ),
-	'finddestination'   => array( '0', 'bestemmingzoeken', 'finddestination' ),
-	'coordinates'       => array( '0', 'coordinaten', 'coordinates' ),
-	'distance'          => array( '0', 'afstand', 'distance' ),
+	'display_map' => array( 0, 'kaart_weergeven' ),
+	'display_point' => array( 0, 'punt_weergeven' ),
+	'geocode' => array( 0, 'geocoderen' ),
+	'geodistance' => array( 0, 'geoafstand' ),
+	'finddestination' => array( 0, 'bestemmingzoeken' ),
+	'coordinates' => array( 0, 'coordinaten' ),
+	'distance' => array( 0, 'afstand' ),
 );
 
+/** Pashto (پښتو) */
 $magicWords['ps'] = array(
-	'distance'          => array( '0', 'واټن', 'distance' ),
+	'distance' => array( 0, 'واټن', 'distance' ),
 );
 
+/** Portuguese (Português) */
 $magicWords['pt'] = array(
-	'display_map'       => array( '0', 'mostrar_mapa', 'display_map' ),
-	'display_point'     => array( '0', 'mostrar_ponto', 'mostrar_pontos', 'display_point', 'display_points' ),
-	'geocode'           => array( '0', 'geocódigo', 'geocode' ),
-	'geodistance'       => array( '0', 'geodistância', 'geodistance' ),
-	'finddestination'   => array( '0', 'encontrardestino', 'finddestination' ),
-	'coordinates'       => array( '0', 'coordenadas', 'coordinates' ),
-	'distance'          => array( '0', 'distância', 'distance' ),
+	'display_map' => array( 0, 'mostrar_mapa', 'display_map' ),
+	'display_point' => array( 0, 'mostrar_ponto', 'mostrar_pontos', 'display_point', 'display_points' ),
+	'geocode' => array( 0, 'geocódigo', 'geocode' ),
+	'geodistance' => array( 0, 'geodistância', 'geodistance' ),
+	'finddestination' => array( 0, 'encontrardestino', 'finddestination' ),
+	'coordinates' => array( 0, 'coordenadas', 'coordinates' ),
+	'distance' => array( 0, 'distância', 'distance' ),
 );
 
+/** Russian (Русский) */
 $magicWords['ru'] = array(
-	'display_map'       => array( '0', 'показать_карту', 'display_map' ),
-	'display_point'     => array( '0', 'показать_точку', 'display_point', 'display_points' ),
-	'geocode'           => array( '0', 'геокод', 'geocode' ),
-	'geodistance'       => array( '0', 'георасстояние', 'geodistance' ),
-	'finddestination'   => array( '0', 'найти_местоположение', 'finddestination' ),
-	'coordinates'       => array( '0', 'координаты', 'coordinates' ),
-	'distance'          => array( '0', 'расстояние', 'distance' ),
+	'display_map' => array( 0, 'показать_карту', 'display_map' ),
+	'display_point' => array( 0, 'показать_точку', 'display_point', 'display_points' ),
+	'geocode' => array( 0, 'геокод', 'geocode' ),
+	'geodistance' => array( 0, 'георасстояние', 'geodistance' ),
+	'finddestination' => array( 0, 'найти_местоположение', 'finddestination' ),
+	'coordinates' => array( 0, 'координаты', 'coordinates' ),
+	'distance' => array( 0, 'расстояние', 'distance' ),
 );
 
+/** Sinhala (සිංහල) */
 $magicWords['si'] = array(
-	'finddestination'   => array( '0', 'ගමනාන්තය_සෙවීම', 'finddestination' ),
-	'coordinates'       => array( '0', 'ඛණ්ඩාංක', 'coordinates' ),
-	'distance'          => array( '0', 'දුර', 'distance' ),
+	'finddestination' => array( 0, 'ගමනාන්තය_සෙවීම', 'finddestination' ),
+	'coordinates' => array( 0, 'ඛණ්ඩාංක', 'coordinates' ),
+	'distance' => array( 0, 'දුර', 'distance' ),
+);
+
+/** Vietnamese (Tiếng Việt) */
+$magicWords['vi'] = array(
+	'coordinates' => array( 0, 'tọađộ' ),
 );
