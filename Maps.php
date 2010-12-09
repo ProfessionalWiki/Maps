11c15c3edef9b63a666df1ed609b24d451975771
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-09 11:57:55 +0000

    Follow-up r78122: Remove wfLoadExtensionMessages from selected extensions (those run on translatewiki.net). * special handling for Maps and Validator: check MediaWiki version and if 1.15 of lower, call wfLoadExtensionMessages.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@78123 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.php
+++ Maps.php
@@ -160,11 +160,11 @@ else {
  * @return true
  */
 function efMapsSetup() {
-	global $wgExtensionCredits, $wgLang, $wgExtraNamespaces, $wgNamespaceAliases;
+	global $wgExtensionCredits, $wgLang, $wgExtraNamespaces, $wgNamespaceAliases, $wgVersion;
 
 	// This function has been deprecated in 1.16, but needed for earlier versions.
 	// It's present in 1.16 as a stub, but lets check if it exists in case it gets removed at some point.
-	if ( function_exists( 'wfLoadExtensionMessages' ) ) {
+	if ( version_compare( $wgVersion, '1.15', '<=' ) ) {
 		wfLoadExtensionMessages( 'Maps' );
 	}
 
