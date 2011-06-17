a70afd5e372475e8a64548dfc97d9a36a995d3d0
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-17 16:27:04 +0000

    Swap else if for elseif
    Trimming trailing whitespace also
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@90287 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/manipulations/Maps_ParamDimension.php
+++ includes/manipulations/Maps_ParamDimension.php
@@ -2,42 +2,42 @@
 
 /**
  * Parameter manipulation ensuring the value is width or height.
- * 
+ *
  * @since 0.7
- * 
+ *
  * @file Maps_ParamDimension.php
  * @ingroup Maps
  * @ingroup ParameterManipulations
- * 
+ *
  * @author Jeroen De Dauw
  */
 class MapsParamDimension extends ItemParameterManipulation {
-	
+
 	/**
 	 * String indicating if this is for a width or a height.
-	 * 
+	 *
 	 * @since 0.7
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $dimension;
-	
+
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @since 0.7
 	 */
 	public function __construct( $dimension ) {
 		parent::__construct();
-		
-		$this->dimension = $dimension;		
+
+		$this->dimension = $dimension;
 	}
-	
+
 	/**
 	 * @see ItemParameterManipulation::doManipulation
-	 * 
+	 *
 	 * @since 0.7
-	 */	
+	 */
 	public function doManipulation( &$value, Parameter $parameter, array &$parameters ) {
 		global $egMapsSizeRestrictions, $egMapsMapWidth, $egMapsMapHeight;
 
@@ -70,13 +70,13 @@ class MapsParamDimension extends ItemParameterManipulation {
 		$number = preg_replace( '/[^0-9]/', '', $value );
 		if ( $number < $min ) {
 			$value = $min;
-		} else if ( $number > $max ) {
+		} elseif ( $number > $max ) {
 			$value = $max;
 		}
-		
-		if ( !preg_match( '/(px|ex|em|%)$/', $value ) ) { 
-			$value .= 'px'; 
+
+		if ( !preg_match( '/(px|ex|em|%)$/', $value ) ) {
+			$value .= 'px';
 		}
 	}
-	
+
 }
