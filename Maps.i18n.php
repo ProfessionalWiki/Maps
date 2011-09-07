be8520380f4db49dc169edd4e5d6af862f718e9d
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-07 19:15:44 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@96473 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.i18n.php
+++ Maps.i18n.php
@@ -3900,6 +3900,7 @@ $messages['vep'] = array(
  */
 $messages['vi'] = array(
 	'maps-desc' => 'Cung cấp khả năng nhúng bản đồ vào trang wiki, mã hóa địa lý các địa chỉ, và thực hiện các tác vụ địa lý khác ([http://mapping.referata.com/wiki/Examples thử xem])',
+	'right-geocode' => 'Mã hóa địa lý',
 	'maps_map' => 'Bản đồ',
 	'maps-loading-map' => 'Đang tải bản đồ…',
 	'maps-load-failed' => 'Không thể tải bản đồ!',
