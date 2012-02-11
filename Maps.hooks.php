eb5b1052920d7016eb9904596eeabd8f4687d242
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-11 12:17:58 +0000

    * Remove last remaining traces of wfLoadExtensionMessages(). * Add missing action message and remove unused global in UploadLocal. * Some whitespace updates in Maps.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Maps@111231 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Maps.hooks.php
+++ Maps.hooks.php
@@ -2,20 +2,20 @@
 
 /**
  * Static class for hooks handled by the Maps extension.
- * 
+ *
  * @since 0.7
- * 
+ *
  * @file Maps.hooks.php
  * @ingroup Maps
- * 
+ *
  * @licence GNU GPL v3
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 final class MapsHooks {
-	
+
 	/**
 	 * Adds a link to Admin Links page.
-	 * 
+	 *
 	 * @since 0.7
 	 *
 	 * @param ALTree $admin_links_tree
@@ -23,36 +23,36 @@ final class MapsHooks {
 	 * @return true
 	 */
 	public static function addToAdminLinks( ALTree &$admin_links_tree ) {
-	    $displaying_data_section = $admin_links_tree->getSection( wfMsg( 'smw_adminlinks_displayingdata' ) );
-	
-	    // Escape if SMW hasn't added links.
-	    if ( is_null( $displaying_data_section ) ) return true;
-	    $smw_docu_row = $displaying_data_section->getRow( 'smw' );
-	
-	    $maps_docu_label = wfMsg( 'adminlinks_documentation', 'Maps' );
-	    $smw_docu_row->addItem( AlItem::newFromExternalLink( 'http://mapping.referata.com/wiki/Maps', $maps_docu_label ) );
-	
-	    return true;
+		$displaying_data_section = $admin_links_tree->getSection( wfMsg( 'smw_adminlinks_displayingdata' ) );
+
+		// Escape if SMW hasn't added links.
+		if ( is_null( $displaying_data_section ) ) return true;
+		$smw_docu_row = $displaying_data_section->getRow( 'smw' );
+
+		$maps_docu_label = wfMsg( 'adminlinks_documentation', 'Maps' );
+		$smw_docu_row->addItem( AlItem::newFromExternalLink( 'http://mapping.referata.com/wiki/Maps', $maps_docu_label ) );
+
+		return true;
 	}
-	
+
 	/**
 	 * Hook to add PHPUnit test cases.
-	 * 
+	 *
 	 * @since 0.7
-	 * 
+	 *
 	 * @param array $files
 	 *
 	 * @return true
 	 */
 	public static function registerUnitTests( array &$files ) {
 		$testDir = dirname( __FILE__ ) . '/test/';
-		
+
 		$files[] = $testDir . 'MapsCoordinateParserTest.php';
 		$files[] = $testDir . 'MapsDistanceParserTest.php';
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Intercept pages in the Layer namespace to handle them correctly.
 	 *
@@ -65,10 +65,10 @@ final class MapsHooks {
 		if ( $title->getNamespace() == Maps_NS_LAYER ) {
 			$article = new MapsLayerPage( $title );
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Adds global JavaScript variables.
 	 *
@@ -80,14 +80,14 @@ final class MapsHooks {
 	 */
 	public static function onMakeGlobalVariablesScript( array &$vars ) {
 		global $egMapsGlobalJSVars;
-		
+
 		$vars['egMapsDebugJS'] = $GLOBALS['egMapsDebugJS'];
-		
+
 		$vars += $egMapsGlobalJSVars;
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * @since 0.7
 	 *
@@ -100,4 +100,4 @@ final class MapsHooks {
 		$list[Maps_NS_LAYER_TALK] = 'Layer_talk';
 		return true;
 	}
-} 
+}
