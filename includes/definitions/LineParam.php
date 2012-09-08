6d89c6f7f24c4dc27581e5c17554e5485846372f
jeroendedauw <jeroendedauw@gmail.com>
2012-09-08 23:28:05 +0200

    removed distance and layer criteria
    Change-Id: I5f04aa4bf8a995bdaf9fdfe318a9a4b50293d99c

--- includes/definitions/LineParam.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: jeroen
- * Date: 9/1/12
- * Time: 7:50 PM
- * To change this template use File | Settings | File Templates.
- */
---
+++ includes/parsers/DistanceParser.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Maps;
+use StringValueParser, ValueParserResult, ValueParserResultObject;
+
+/**
+ * ValueParser that parses the string representation of a distance.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 2.0
+ *
+ * @file
+ * @ingroup Maps
+ * @ingroup ValueParser
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class DistanceParser extends StringValueParser {
+
+	/**
+	 * @see StringValueParser::stringParse
+	 *
+	 * @since 0.1
+	 *
+	 * @param string $value
+	 *
+	 * @return ValueParserResult
+	 */
+	public function stringParse( $value ) {
+		$distance = \MapsDistanceParser::parseDistance( $value );
+
+		if ( $distance === false ) {
+			return $this->newErrorResult( 'Not a distance' );
+		}
+		else {
+			return ValueParserResultObject::newSuccess( $value );
+		}
+	}
+
+}
