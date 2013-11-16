4836ceca31f40ba42bab10298f09ba0b628e6fb9
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-11-16 03:30:02 +0100

    Update Maps_Settings.php

--- Maps_Settings.php
+++ Maps_Settings.php
@@ -1,7 +1,7 @@
 <?php
+
 /**
  * File defining the settings for the Maps extension.
- * More info can be found at http://mapping.referata.com/wiki/Help:Configuration
  *
  *                          NOTICE:
  * Changing one of these settings can be done by copieng or cutting it,
