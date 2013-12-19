b415df85f8f2545366411a205534fd1ca2e4e361
jeroendedauw <jeroendedauw@gmail.com>
2013-12-19 18:56:54 +0100

    Imrpove PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,14 @@
 
 require_once( __DIR__ . '/evilMediaWikiBootstrap.php' );
 
-echo exec( 'composer update' ) . "\n";
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'Not an entry point' );
+}
+
+$pwd = exec( 'pwd' );
+chdir( __DIR__ . '/..' );
+passthru( 'composer update' );
+chdir( $pwd );
 
 require_once( __DIR__ . '/../vendor/autoload.php' );
 
