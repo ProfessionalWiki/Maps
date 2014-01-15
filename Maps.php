640fa8af22ed897d62516cddfaf93b7ad72f331c
jeroendedauw <jeroendedauw@gmail.com>
2014-01-16 00:55:49 +0100

    Bump to 3.0 RC2

--- Maps.php
+++ Maps.php
@@ -17,7 +17,7 @@ if ( defined( 'Maps_VERSION' ) ) {
 	return 1;
 }
 
-define( 'Maps_VERSION' , '3.0-RC1' );
+define( 'Maps_VERSION' , '3.0-RC2' );
 
 // Include the composer autoloader if it is present.
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
