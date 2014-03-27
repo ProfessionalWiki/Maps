58eaf2eba2686434909fba6a4e5d4ea7fa9284d0
jeroendedauw <jeroendedauw@gmail.com>
2014-03-27 05:41:54 +0100

    Fix file name in Travis config

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -6,7 +6,7 @@ cd ../phase3/extensions/Maps
 
 if [ "$TYPE" == "coverage" ]
 then
-	phpunit --coverage-clover ../../extensions/Maps/build/clover.clover
+	phpunit --coverage-clover ../../extensions/Maps/build/coverage.clover
 else
 	phpunit
 fi
