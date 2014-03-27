997699b2db05aed2e1bfcb9caf941c1ffa1f360e
jeroendedauw <jeroendedauw@gmail.com>
2014-03-27 05:40:51 +0100

    aaaaa

--- build/travis/after_success.sh
+++ build/travis/after_success.sh
@@ -7,5 +7,7 @@ originalDirectory=$(pwd)
 if [ "$TYPE" == "coverage" ]
 then
 	wget https://scrutinizer-ci.com/ocular.phar
+	echo $originalDirectory/build/coverage.clover
+	du -hs $originalDirectory/build/coverage.clover
 	php ocular.phar code-coverage:upload --format=php-clover $originalDirectory/build/coverage.clover
 fi
