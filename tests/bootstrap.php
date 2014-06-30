6548037ae1f1c003d44133c8cac2113457eaac78
jeroendedauw <jeroendedauw@gmail.com>
2014-06-30 12:21:07 +0200

    Update PHPUnit bootstra

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,14 +4,9 @@ if ( php_sapi_name() !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-$pwd = getcwd();
-chdir( __DIR__ . '/..' );
-passthru( 'composer update' );
-chdir( $pwd );
-
 require_once( __DIR__ . '/evilMediaWikiBootstrap.php' );
 
-require_once( __DIR__ . '/../vendor/autoload.php' );
+require_once( __DIR__ . '/../../../vendor/autoload.php' );
 
 foreach ( $GLOBALS['wgExtensionFunctions'] as $extensionFunction ) {
 	call_user_func( $extensionFunction );
