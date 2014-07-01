66e71736d66c177dc8b3038e7e3241bf52ff3573
jeroendedauw <jeroendedauw@gmail.com>
2014-07-01 10:45:25 +0200

    Use new SMW test autoloader

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'Not an entry point' );
+}
+
+if ( is_readable( $path = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
+	print( "\nUsing SemanticMediaWiki ...\n" );
+} else {
+	die( 'The SemanticMediaWiki test autoloader is not available' );
+}
+
+$autoloader = require $path;
+
