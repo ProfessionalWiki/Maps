c7b580f2bbcf6c72e70c05d9aac19cf5f90c9f04
jeroendedauw <jeroendedauw@gmail.com>
2014-07-24 03:01:28 +0200

    fix typo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ if ( php_sapi_name() !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-if ( is_readable( $path = __DIR__ . '/extenions/SemanticMediaWiki/tests/autoloader.php' ) ) {
+if ( is_readable( $path = __DIR__ . '/extensions/SemanticMediaWiki/tests/autoloader.php' ) ) {
 	print( "\nUsing SemanticMediaWiki ...\n" );
 } else {
 	die( 'The SemanticMediaWiki test autoloader is not available' );
