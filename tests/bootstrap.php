4cf65da680189aa5cc7a768812f3a86d63d2fbc6
jeroendedauw <jeroendedauw@gmail.com>
2014-08-05 20:55:04 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,11 @@ if ( php_sapi_name() !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-if ( is_readable( $path = __DIR__ . '/../../extensions/SemanticMaps/extensions/SemanticMediaWiki/tests/autoloader.php' ) ) {
+if ( is_readable( $path = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
 	print( "\nUsing SemanticMediaWiki ...\n" );
 } else {
 	die( 'The SemanticMediaWiki test autoloader is not available' );
 }
 
-$autoloader = require $path;
+require $path;
 
