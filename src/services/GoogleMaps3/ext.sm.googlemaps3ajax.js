e89663c2289f4c2d7a9a5213d7e114f1ecc3f675
PeterTheOne <petertheone@gmail.com>
2016-03-25 12:35:55 +0100

    add ajax request
    (depends on small changes in Maps)

--- src/services/GoogleMaps3/ext.sm.googlemaps3ajax.js
+++ src/services/GoogleMaps3/ext.sm.googlemaps3ajax.js
@@ -11,33 +11,50 @@
 
         console.log('test init');
 
-        // todo: trigger fetch once on load.
-        // todo: add 'zoom_changed'
-        /*google.maps.event.addListener(map, 'dragend', function () {
-            var bounds = map.getBounds();
+        if ( typeof google !== 'undefined' ) {
+            $( '.maps-googlemaps3' ).each( function() {
+                var $this = $( this );
 
-            $.ajax({
-                method: 'GET',
-                url: '/w/api.php?',
-                data: {
-                    'action': 'coordinates',
-                    'locations': '',
-                    'bbSouth': bounds.getSouthWest().lat,
-                    'bbWest': bounds.getSouthWest().lng,
-                    'bbNorth': bounds.getNorthEast().lat,
-                    'bbEast': bounds.getNorthEast().lng,
-                    'format': 'json'
-                },
-                dataType: 'json'
-            }).done(function(data) {
-                // todo: don't add the same marker multiple times
-                // maybe remove all old markers
-                for (var i = data.results.locations.length - 1; i >= 0; i--) {
-                    self.addMarker(data.results.locations[i]);
-                }
+                // todo: trigger fetch once on load.
+                // todo: add 'zoom_changed'
+
+                google.maps.event.addListener($this.googlemaps.map, 'dragend', function () {
+                    var bounds = $this.googlemaps.map.getBounds();
+
+                    var query = '[[Category:Locations]]' + ' ';
+                    query += '[[Has coordinates::+]] ';
+                    query += '[[Has coordinates::>' + bounds.getSouthWest().lat() + '°, ' + bounds.getSouthWest().lng() + '°]] ';
+                    query += '[[Has coordinates::<' + bounds.getNorthEast().lat() + '°, ' + bounds.getNorthEast().lng() + '°]]';
+                    query += '|?Has coordinates';
+
+                    $.ajax({
+                        method: 'GET',
+                        url: '/w/api.php?',
+                        data: {
+                            'action': 'ask',
+                            'query': query,
+                            'format': 'json'
+                        },
+                        dataType: 'json'
+                    }).done(function(data) {
+                        // todo: don't add the same marker multiple times
+                        // maybe remove all old markers
+                        for (var property in data.query.results) {
+                            if (data.query.results.hasOwnProperty(property)) {
+                                var location = data.query.results[property];
+                                var coordinates = location.printouts['Has coordinates'][0];
+                                $this.googlemaps.addMarker(coordinates);
+                            }
+                        }
+                    });
+                });
+
+
             });
-        });*/
+        }
 
     } );
+
+
 
 })( window.jQuery, mediaWiki );
