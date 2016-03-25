80c9a204fd796df7bf576f89d6e8285c9cd70711
PeterTheOne <petertheone@gmail.com>
2016-03-25 13:39:42 +0100

    add listener to zoom_changed, update markers on load

--- src/services/GoogleMaps3/ext.sm.googlemaps3ajax.js
+++ src/services/GoogleMaps3/ext.sm.googlemaps3ajax.js
@@ -5,57 +5,56 @@
  * @licence GNU GPL v2+
  * @author Peter Grassberger <petertheone@gmail.com>
  */
+
+function ajaxUpdateMarker(googlemaps) {
+    var bounds = googlemaps.map.getBounds();
+
+    var query = '[[Category:Locations]]' + ' ';
+    query += '[[Has coordinates::+]] ';
+    query += '[[Has coordinates::>' + bounds.getSouthWest().lat() + '°, ' + bounds.getSouthWest().lng() + '°]] ';
+    query += '[[Has coordinates::<' + bounds.getNorthEast().lat() + '°, ' + bounds.getNorthEast().lng() + '°]]';
+    query += '|?Has coordinates';
+
+    $.ajax({
+        method: 'GET',
+        url: '/w/api.php?',
+        data: {
+            'action': 'ask',
+            'query': query,
+            'format': 'json'
+        },
+        dataType: 'json'
+    }).done(function(data) {
+        googlemaps.removeMarkers();
+        for (var property in data.query.results) {
+            if (data.query.results.hasOwnProperty(property)) {
+                var location = data.query.results[property];
+                var coordinates = location.printouts['Has coordinates'][0];
+                googlemaps.addMarker(coordinates);
+            }
+        }
+    });
+}
+
 (function( $, mw ) {
-
     $( document ).ready( function() {
-
-        console.log('test init');
-
         if ( typeof google !== 'undefined' ) {
             $( '.maps-googlemaps3' ).each( function() {
                 var $this = $( this );
 
-                // todo: trigger fetch once on load.
-                // todo: add 'zoom_changed'
+                // todo: remove timeout
+                setTimeout(function() {
+                    ajaxUpdateMarker($this.googlemaps);
+                }, 100);
 
                 google.maps.event.addListener($this.googlemaps.map, 'dragend', function () {
-                    var bounds = $this.googlemaps.map.getBounds();
-
-                    var query = '[[Category:Locations]]' + ' ';
-                    query += '[[Has coordinates::+]] ';
-                    query += '[[Has coordinates::>' + bounds.getSouthWest().lat() + '°, ' + bounds.getSouthWest().lng() + '°]] ';
-                    query += '[[Has coordinates::<' + bounds.getNorthEast().lat() + '°, ' + bounds.getNorthEast().lng() + '°]]';
-                    query += '|?Has coordinates';
-
-                    $.ajax({
-                        method: 'GET',
-                        url: '/w/api.php?',
-                        data: {
-                            'action': 'ask',
-                            'query': query,
-                            'format': 'json'
-                        },
-                        dataType: 'json'
-                    }).done(function(data) {
-                        // todo: don't add the same marker multiple times
-                        // maybe remove all old markers
-                        $this.googlemaps.removeMarkers();
-                        for (var property in data.query.results) {
-                            if (data.query.results.hasOwnProperty(property)) {
-                                var location = data.query.results[property];
-                                var coordinates = location.printouts['Has coordinates'][0];
-                                $this.googlemaps.addMarker(coordinates);
-                            }
-                        }
-                    });
+                    ajaxUpdateMarker($this.googlemaps);
                 });
-
+                google.maps.event.addListener($this.googlemaps.map, 'zoom_changed', function () {
+                    ajaxUpdateMarker($this.googlemaps);
+                });
 
             });
         }
-
     } );
-
-
-
 })( window.jQuery, mediaWiki );
