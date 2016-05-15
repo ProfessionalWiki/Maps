d2d32e0b8462b783a9e0462baeac2fddbd3c3152
PeterTheOne <petertheone@gmail.com>
2016-05-15 12:51:38 +0200

    extract function getQueryString and fix code style

--- src/services/GoogleMaps3/ext.sm.googlemaps3ajax.js
+++ src/services/GoogleMaps3/ext.sm.googlemaps3ajax.js
@@ -8,22 +8,26 @@
 
 var ajaxRequest = null;
 
-function ajaxUpdateMarker(map) {
+function getQueryString( map, coordinatesproperty ) {
     var bounds = map.map.getBounds();
 
-    var coordinatesproperty = map.options.coordinatesproperty;
-
-    var query = map.options.ajaxquery.join(' ') + ' ';
+    var query = map.options.ajaxquery.join( ' ' ) + ' ';
     query += '[[' + coordinatesproperty + '::+]] ';
     query += '[[' + coordinatesproperty + '::>' + bounds.getSouthWest().lat() + '°, ' + bounds.getSouthWest().lng() + '°]] ';
     query += '[[' + coordinatesproperty + '::<' + bounds.getNorthEast().lat() + '°, ' + bounds.getNorthEast().lng() + '°]]';
     query += '|?' + coordinatesproperty;
+    return query;
+}
 
-    if (ajaxRequest !== null) {
+function ajaxUpdateMarker( map ) {
+    var coordinatesproperty = map.options.coordinatesproperty;
+    var query = getQueryString( map, coordinatesproperty );
+
+    if ( ajaxRequest !== null ) {
         ajaxRequest.abort();
     }
 
-    ajaxRequest = $.ajax({
+    ajaxRequest = $.ajax( {
         method: 'GET',
         url: '/w/api.php?',
         data: {
@@ -32,23 +36,23 @@ function ajaxUpdateMarker(map) {
             'format': 'json'
         },
         dataType: 'json'
-    }).done(function(data) {
+    } ).done( function( data ) {
         ajaxRequest = null;
 
         // todo: don't remove and recreate all markers..
         // only add new ones.
         map.removeMarkers();
-        for (var property in data.query.results) {
-            if (data.query.results.hasOwnProperty(property)) {
+        for ( var property in data.query.results ) {
+            if ( data.query.results.hasOwnProperty( property ) ) {
                 var location = data.query.results[property];
                 var coordinates = location.printouts[coordinatesproperty][0];
-                map.addMarker(coordinates);
+                map.addMarker( coordinates );
             }
         }
-    });
+    } );
 }
 
-(function( $, mw ) {
+(function( $ ) {
     var events = ['dragend', 'zoom_changed'];
 
     $( document ).ready( function() {
@@ -57,16 +61,16 @@ function ajaxUpdateMarker(map) {
             if ( typeof google === 'undefined' ) {
                 return;
             }
-            $(window.maps.googlemapList).each( function(index, map) {
-                if (!map.options.ajaxquery) {
+            $( window.maps.googlemapList ).each( function( index, map ) {
+                if (!map.options.ajaxquery && !map.options.coordinatesproperty) {
                     return;
                 }
-                $(events).each(function(index, event) {
-                    google.maps.event.addListener(map.map, event, function () {
-                        ajaxUpdateMarker(map);
-                    });
-                });
-            });
-        }, 500);
+                $( events ).each( function( index, event ) {
+                    google.maps.event.addListener( map.map, event, function () {
+                        ajaxUpdateMarker( map );
+                    } );
+                } );
+            } );
+        }, 500 );
     } );
-})( window.jQuery, mediaWiki );
+})( window.jQuery );
