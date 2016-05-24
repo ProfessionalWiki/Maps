2ba4dcf480b407c7fde4fd84a268c224e8513a79
PeterTheOne <petertheone@gmail.com>
2016-05-24 20:40:03 +0200

    cleanup MapsMapperTest

--- tests/phpunit/MapsMapperTest.php
+++ tests/phpunit/MapsMapperTest.php
@@ -14,31 +14,32 @@ use MapsMapper;
  * @licence GNU GPL v2+
  * @author Peter Grassberger < petertheone@gmail.com >
  */
-class MapsMapperTest extends \MediaWikiTestCase {
+class MapsMapperTest extends \PHPUnit_Framework_TestCase {
 
-    public static $imageUrls = [
-        'markerImage.png' => 'markerImage.png',
-        '/w/images/c/ce/Green_marker.png' => '/w/images/c/ce/Green_marker.png',
-        '//semantic-mediawiki.org/w/images/c/ce/Green_marker.png' => '//semantic-mediawiki.org/w/images/c/ce/Green_marker.png',
-        'Cat2.jpg' => 'Cat2.jpg'
-    ];
+    public function imageUrlProvider() {
+        return [
+            ['markerImage.png', 'markerImage.png'],
+            ['/w/images/c/ce/Green_marker.png', '/w/images/c/ce/Green_marker.png'],
+            ['//semantic-mediawiki.org/w/images/c/ce/Green_marker.png', '//semantic-mediawiki.org/w/images/c/ce/Green_marker.png'],
+            ['Cat2.jpg', 'Cat2.jpg'],
+        ];
+    }
 
-    function getImagePage( $filename ) {
-        $title = Title::makeTitleSafe( NS_FILE, $filename );
-        $file = $this->dataFile( $filename );
-        $iPage = new ImagePage( $title );
-        $iPage->setFile( $file );
-        return $iPage;
+    /**
+     * Tests MapsMapperTest::getFileUrl()
+     *
+     * @dataProvider imageUrlProvider
+     */
+    public function testGetFileUrl($file, $expected) {
+        $this->assertSame( $expected, MapsMapper::getFileUrl($file) );
     }
 
     /**
      * Tests MapsMapperTest::getFileUrl()
      */
-    public function testGetFileUrl() {
-        foreach ( self::$imageUrls as $rawValue => $parsedValue ) {
-            $this->assertEquals( $parsedValue, MapsMapper::getFileUrl($rawValue) );
-        }
-        $this->assertEquals( null, MapsMapper::getFileUrl(null));
-        // TODO test with existing imagePage
+    public function testGivenNull_getFileUrlReturnsNull() {
+        $this->assertNull( MapsMapper::getFileUrl(null));
     }
+
+    // TODO test with existing imagePage
 }
