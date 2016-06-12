5fa50dda04ad97ea537a39aeea43ff6677299c36
Jeroen De Dauw <jeroendedauw@gmail.com>
2016-06-12 13:00:52 +0200

    Update Maps.php
    [skip ci]

--- Maps.php
+++ Maps.php
@@ -19,7 +19,7 @@ if ( defined( 'Maps_VERSION' ) ) {
 	return 1;
 }
 
-define( 'Maps_VERSION' , '3.6.0' );
+define( 'Maps_VERSION' , '3.7.0 alpha' );
 
 // Include the composer autoloader if it is present.
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
