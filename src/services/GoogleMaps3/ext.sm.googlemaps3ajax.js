88741f365023513c7667b1ccf09cdf8237a55ae1
PeterTheOne <petertheone@gmail.com>
2016-08-08 12:30:46 +0200

    recreate marker cluster on leaflet ajax

--- src/services/GoogleMaps3/ext.sm.googlemaps3ajax.js
+++ src/services/GoogleMaps3/ext.sm.googlemaps3ajax.js
@@ -36,7 +36,7 @@
                         if ( ajaxRequest !== null ) {
                             ajaxRequest.abort();
                         }
-                        ajaxRequest = sm.ajaxUpdateMarker( map, query).done( function () {
+                        ajaxRequest = sm.ajaxUpdateMarker( map, query ).done( function () {
                             ajaxRequest = null;
                         } );
                     } );
