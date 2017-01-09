5ac675b2d96ebff2cb605d50c14216e8e305ac7d
Абрамов Виталий <someuser@comandor.local>
2017-01-09 15:30:36 +0700

    Maps_GoogleGeocoder - address being double urlencoded

--- includes/geocoders/Maps_GoogleGeocoder.php
+++ includes/geocoders/Maps_GoogleGeocoder.php
@@ -36,7 +36,7 @@ final class MapsGoogleGeocoder extends \Maps\Geocoder {
 	 */	
 	protected function getRequestUrl( $address ) {
 		$urlArgs = [
-			'address' => urlencode( $address )
+			'address' => $address
 		];
 		if ( $GLOBALS['egMapsGMaps3ApiKey'] !== '' ) {
 			$urlArgs['key'] = $GLOBALS['egMapsGMaps3ApiKey'];
