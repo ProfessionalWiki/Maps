d4f66be95937eb9133a42332735ae118ce7f1ec9
HgO <hgo@batato.be>
2017-09-27 22:07:46 +0200

    use a keyspace when accessing entries in the cache

--- src/Geocoders/CachingGeocoder.php
+++ src/Geocoders/CachingGeocoder.php
@@ -11,15 +11,10 @@ use DataValues\Geo\Values\LatLongValue;
  * @author HgO < hgo@batato.be >
  */
 class CachingGeocoder implements Geocoder {
-	protected $geocoder;
+	private $geocoder;
 	
-	protected $cache;
+	private $cache;
 
-	/**
-	 * @param Geocoder $geocoder
-	 * @param BagOStuff $cache
-	 *
-	 */
 	public function __construct( $geocoder, $cache ) {
 		$this->geocoder = $geocoder;
 		$this->cache = $cache;
@@ -31,13 +26,15 @@ class CachingGeocoder implements Geocoder {
 	 * @return LatLongValue|null
 	 */
 	public function geocode( $address ) {
-		$coordinates = $this->cache->get( $address );
+		$key = $this->cache->makeKey( __CLASS__, $address );
+		
+		$coordinates = $this->cache->get( $key );
 		
 		// There was no entry in the cache, so we retrieve the coordinates
 		if ( $coordinates === false ) {
 			$coordinates = $this->geocoder->geocode( $address );
 			
-			$this->cache->set( $address, $coordinates, $this->cache::TTL_DAY );
+			$this->cache->set( $key, $coordinates, $this->cache::TTL_DAY );
 		}
 		
 		return $coordinates;
