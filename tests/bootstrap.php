6a5c72116cf2263a1c51e51361afaec28e1ef124
Kunal Mehta <legoktm@member.fsf.org>
2018-05-16 19:00:06 -0700

    Make tests compatible with PHPUnit 4 and 6
    Use the PHPUnit4And6Compat trait from MediaWiki core to make PHPUnit 4
    methods work even when being run with PHPUnit 6. If it's being run with
    an older MediaWiki core version, define a empty trait in the bootstrap
    to avoid fatal errors. Since the lack of the trait means we're running
    with PHPUnit 4, everything should work out.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,4 +15,11 @@ if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
 
+// If testing against an older version of MediaWiki, define
+// an empty trait to avoid fatal errors.
+if ( !trait_exists( PHPUnit4And6Compat::class ) ) {
+	trait PHPUnit4And6Compat {
+	}
+}
+
 require __DIR__ . '/../vendor/autoload.php';
