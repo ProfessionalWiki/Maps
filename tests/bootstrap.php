82f7c62c7679dcba2a04852442a76e33e7705ee6
Kunal Mehta <legoktm@member.fsf.org>
2018-05-16 19:04:13 -0700

    Move trait definition up in bootstrap so it runs with MediaWiki

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 
+// If testing against an older version of MediaWiki, define
+// an empty trait to avoid fatal errors.
+if ( !trait_exists( PHPUnit4And6Compat::class ) ) {
+	trait PHPUnit4And6Compat {
+	}
+}
+
 if ( defined( 'MEDIAWIKI' ) ) {
 	return;
 }
@@ -13,13 +20,6 @@ ini_set( 'display_errors', 1 );
 
 if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
-}
-
-// If testing against an older version of MediaWiki, define
-// an empty trait to avoid fatal errors.
-if ( !trait_exists( PHPUnit4And6Compat::class ) ) {
-	trait PHPUnit4And6Compat {
-	}
 }
 
 require __DIR__ . '/../vendor/autoload.php';
