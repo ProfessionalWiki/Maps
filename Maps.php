c435b1ad4f9a202246317f15bded6c96c7465e24
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-07-14 18:59:58 +0200

    Update Maps.php
    [skip ci]

--- Maps.php
+++ Maps.php
@@ -67,7 +67,7 @@ $GLOBALS['wgExtensionFunctions'][] = function() {
 		);
 	}
 
-	define( 'Maps_VERSION', '5.5.5' );
+	define( 'Maps_VERSION', '5.6 alpha' );
 	define( 'SM_VERSION', Maps_VERSION );
 
 	if ( $GLOBALS['egMapsGMaps3Language'] === '' ) {
