67e31af867be77b026684c8767fb9e205c81f8ce
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-07-16 15:15:51 +0200

    Fix imageoverlays parameter
    Fixes https://github.com/JeroenDeDauw/Maps/issues/450

--- tests/Unit/Elements/ImageOverlayTest.php
+++ tests/Unit/Elements/ImageOverlayTest.php
@@ -3,7 +3,10 @@
 namespace Maps\Tests\Elements;
 
 use DataValues\Geo\Values\LatLongValue;
+use Jeroen\SimpleGeocoder\Geocoders\Decorators\CoordinateFriendlyGeocoder;
+use Jeroen\SimpleGeocoder\Geocoders\NullGeocoder;
 use Maps\Elements\ImageOverlay;
+use Maps\ImageOverlayParser;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -22,6 +25,25 @@ class ImageOverlayTest extends TestCase {
 		);
 
 		$this->assertSame( 'Foo.png', $imageOverlay->getImage() );
+	}
+
+	public function testGivenMetaData_overlayHasProvidedMetaData() {
+		$parser = new ImageOverlayParser( new CoordinateFriendlyGeocoder( new NullGeocoder() ) );
+
+		$overlay = $parser->parse( "1,2:3,4:https://such.an/image.png~Semantic MediaWiki~World domination imminent!~https://such.link" );
+
+		$this->assertSame( 'https://such.an/image.png', $overlay->getImage() );
+		$this->assertSame( 'Semantic MediaWiki', $overlay->getTitle() );
+		$this->assertSame( 'World domination imminent!', $overlay->getText() );
+		$this->assertSame( 'https://such.link', $overlay->getLink() );
+	}
+
+	public function testGivenLinkWithPrefix_linkIsParsedAndPrefixIsRemoved() {
+		$parser = new ImageOverlayParser( new CoordinateFriendlyGeocoder( new NullGeocoder() ) );
+
+		$overlay = $parser->parse( "1,2:3,4:https://such.an/image.png~Semantic MediaWiki~World domination imminent!~link:https://such.link" );
+
+		$this->assertSame( 'https://such.link', $overlay->getLink() );
 	}
 
 }
