889447924f46e53696737981194cc2f00bc45264
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-11-19 22:33:56 +0100

    Clean KmlFormatter

--- src/Presentation/KmlFormatter.php
+++ src/Presentation/KmlFormatter.php
@@ -8,34 +8,21 @@ use Xml;
 /**
  * Class to format geographical data to KML.
  *
- * @since 0.7.3
- *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class KmlFormatter {
 
 	/**
-	 * @since 1.0
-	 *
 	 * @var array
 	 */
-	protected $params;
+	private $params;
 
 	/**
-	 * @since 0.7.3
-	 *
 	 * @var Location[]
 	 */
-	protected $placemarks;
+	private $placemarks;
 
-	/**
-	 * Constructor.
-	 *
-	 * @since 0.7.3
-	 *
-	 * @param array $params
-	 */
 	public function __construct( array $params = [] ) {
 		$this->params = $params;
 		$this->clearElements();
@@ -43,8 +30,6 @@ class KmlFormatter {
 
 	/**
 	 * Clears all set geographical objects.
-	 *
-	 * @since 0.7.3
 	 */
 	public function clearElements() {
 		$this->clearPlacemarks();
@@ -52,8 +37,6 @@ class KmlFormatter {
 
 	/**
 	 * Clears all set placemarks.
-	 *
-	 * @since 0.7.3
 	 */
 	public function clearPlacemarks() {
 		$this->placemarks = [];
@@ -61,12 +44,8 @@ class KmlFormatter {
 
 	/**
 	 * Builds and returns KML representing the set geographical objects.
-	 *
-	 * @since 0.7.3
-	 *
-	 * @return string
 	 */
-	public function getKML() {
+	public function getKML(): string {
 		$elements = $this->getKMLElements();
 
 		// http://earth.google.com/kml/2.2
@@ -82,15 +61,11 @@ EOT;
 
 	/**
 	 * Returns the KML for all set geographical objects.
-	 *
-	 * @since 0.7.3
-	 *
-	 * @return string
 	 */
-	protected function getKMLElements() {
+	private function getKMLElements(): string {
 		$elements = [];
 
-		$elements = array_merge( $elements, $this->getPlacemarks() );
+		$elements = array_merge( $elements, $this->getPlaceMarks() );
 
 		return implode( "\n", $elements );
 	}
@@ -98,31 +73,18 @@ EOT;
 	/**
 	 * Returns KML for all set placemarks in a list, where each element is
 	 * a KML node representing a placemark.
-	 *
-	 * @since 0.7.3
-	 *
-	 * @return array
 	 */
-	protected function getPlacemarks() {
-		$placemarks = [];
+	private function getPlaceMarks(): array {
+		$placeMarks = [];
 
 		foreach ( $this->placemarks as $location ) {
-			$placemarks[] = $this->getKMLForLocation( $location );
+			$placeMarks[] = $this->getKMLForLocation( $location );
 		}
 
-		return $placemarks;
+		return $placeMarks;
 	}
 
-	/**
-	 * Returns the KML representing the provided location.
-	 *
-	 * @since 0.7.3
-	 *
-	 * @param Location $location
-	 *
-	 * @return string
-	 */
-	protected function getKMLForLocation( Location $location ) {
+	private function getKMLForLocation( Location $location ): string {
 		$name = '<name><![CDATA[ ' . $location->getTitle() . ']]></name>';
 
 		$description = '<description><![CDATA[ ' . $location->getText() . ']]></description>';
@@ -149,27 +111,12 @@ EOT;
 	}
 
 	/**
-	 * Adds a multiple placemarks.
-	 *
-	 * @since 0.7.3
-	 *
 	 * @param Location[] $placemarks
 	 */
 	public function addPlacemarks( array $placemarks ) {
 		foreach ( $placemarks as $placemark ) {
-			$this->addPlacemark( $placemark );
+			$this->placemarks[] = $placemark;
 		}
-	}
-
-	/**
-	 * Adds a single placemark.
-	 *
-	 * @since 0.7.3
-	 *
-	 * @param Location $placemark
-	 */
-	public function addPlacemark( Location $placemark ) {
-		$this->placemarks[] = $placemark;
 	}
 
 }
