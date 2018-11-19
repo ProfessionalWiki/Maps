d800409a0f4b2022f26efe304966ee43cb43bbe8
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-11-19 20:53:09 +0100

    Remove dead functions

--- includes/ext.maps.common.js
+++ includes/ext.maps.common.js
@@ -1,10 +1,4 @@
 window.maps = new ( function( $, mw ) {
-	
-	this.log = function( message ) {
-		if ( mw.config.get( 'egMapsDebugJS' ) ) {
-			mw.log( message );
-		}
-	};
 
 	this.googlemapsList = [];
 	this.leafletList = [];
