ec3dc57cb53d76c02cdbf43cbd6b18e6a72d877f
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-12-16 01:35:51 +0100

    Revert "Update settings loading"
    This reverts commit b375f058c314cdf231a7e1569ddb88a200c273b9.

--- MapsRegistration.php
+++ MapsRegistration.php
@@ -11,11 +11,7 @@ class MapsRegistration {
 		}
 
 		if ( !defined( 'Maps_SETTINGS_LOADED' ) ) {
-			foreach ( include __DIR__ . '/DefaultSettings.php' as $key => $value ) {
-				$GLOBALS[$key] = $value;
-			}
-
-			define( 'Maps_SETTINGS_LOADED', true );
+			require_once __DIR__ . '/Maps_Settings.php';
 		}
 
 		if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
--- Maps_Settings.php
+++ Maps_Settings.php
@@ -1,4 +1,17 @@
 <?php
 
-// If you are looking for the default settings of the Maps extension,
-// please see the DefaultSettings.php file.
+/**
+ * For instructions on how to configure Maps, see
+ * https://www.semantic-mediawiki.org/wiki/Maps/Configuration
+ *
+ * For a list of all available settings and their default values,
+ * see DefaultSettings.php in this directory.
+ */
+
+foreach ( include __DIR__ . '/DefaultSettings.php' as $key => $value ) {
+	$GLOBALS[$key] = $value;
+}
+
+if ( !defined( 'Maps_SETTINGS_LOADED' ) ) {
+	define( 'Maps_SETTINGS_LOADED', true );
+}
