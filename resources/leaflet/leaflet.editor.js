73794b12fdea06302906076ae9c7f418801cb996
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-09-23 17:10:48 +0200

    Add editor UI to GeoJSON NS

--- resources/leaflet/leaflet.editor.js
+++ resources/leaflet/leaflet.editor.js
@@ -1,7 +1,7 @@
 (function( $, mw ) {
 
 	$( document ).ready( function() {
-		var map = L.map('GeoJsonMap'/*, {editable: true}*/);
+		var map = L.map('GeoJsonMap');
 
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -10,6 +10,29 @@
 		var geoJsonLayer = L.geoJSON(GeoJson).addTo(map);
 
 		map.fitBounds(geoJsonLayer.getBounds());
+
+		var drawnItems = L.featureGroup().addTo(map);
+
+		map.addControl(new L.Control.Draw({
+			edit: {
+				featureGroup: drawnItems,
+				poly: {
+					allowIntersection: false
+				}
+			},
+			draw: {
+				polygon: {
+					allowIntersection: false,
+					showArea: true
+				}
+			}
+		}));
+
+		map.on(L.Draw.Event.CREATED, function (event) {
+			var layer = event.layer;
+
+			drawnItems.addLayer(layer);
+		});
 	} );
 
 })( window.jQuery, mediaWiki );
