12d2cdb0e0a9034b0f73fbdfaad27bfe2a1d1259
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-10-24 04:03:55 +0200

    Use wgVersion instead of MW_VERSION
    Fixes https://github.com/JeroenDeDauw/Maps/issues/585

--- src/MapsSetup.php
+++ src/MapsSetup.php
@@ -212,7 +212,7 @@ class MapsSetup {
 	 * Once Maps requires MW 1.33+, this can be removed after replacing usage of mediawiki.api.edit
 	 */
 	private function registerEditApiModuleFallback() {
-		if ( defined( 'MW_VERSION' ) && version_compare( MW_VERSION, '1.32', '<' ) ) {
+		if ( version_compare( $this->mwGlobals['wgVersion'], '1.32', '<' ) ) {
 			return;
 		}
 
