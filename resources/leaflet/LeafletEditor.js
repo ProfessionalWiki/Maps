bca3d9a4cc0e53b9c3588a0e0159848c7b786412
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-11-21 00:58:32 +0100

    Add leaflet path doc

--- resources/leaflet/LeafletEditor.js
+++ resources/leaflet/LeafletEditor.js
@@ -111,6 +111,7 @@
 
 						// https://github.com/Leaflet/Leaflet/blob/f8e09f993292579a1af88261c9b461730f22e4e6/src/layer/GeoJSON.js#L49-L57
 						// https://github.com/mapbox/simplestyle-spec/tree/master/1.1.0
+						// https://leafletjs.com/reference-1.6.0.html#path
 						if (feature.properties.fill) {
 							pathOptions.fillColor = feature.properties.fill;
 						}
