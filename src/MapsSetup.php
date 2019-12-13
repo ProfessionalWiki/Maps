b7f3a60af5d42b1ca24c71e6d419833090379b8a
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-12-13 01:01:21 +0100

    Improve MediaWiki 1.35 compat

--- src/MapsSetup.php
+++ src/MapsSetup.php
@@ -205,8 +205,8 @@ class MapsSetup {
 	}
 
 	private function registerEditApiModuleFallbacks() {
-		// mediawiki.api.edit is present in 1.31 but not 1.33
-		// Once Maps requires MW 1.33+, this can be removed after replacing usage of mediawiki.api.edit
+		// mediawiki.api.edit is present in 1.31 but not 1.32
+		// Once Maps requires MW 1.32+, this can be removed after replacing usage of mediawiki.api.edit
 		if ( version_compare( $this->mwGlobals['wgVersion'], '1.32', '>=' ) ) {
 			$this->mwGlobals['wgResourceModules']['mediawiki.api.edit'] = [
 				'dependencies' => [
@@ -216,6 +216,7 @@ class MapsSetup {
 			];
 		}
 
+		// 1.35 combines the jquery.ui modules into one
 		if ( version_compare( $this->mwGlobals['wgVersion'], '1.35', '>=' ) ) {
 			$this->mwGlobals['wgResourceModules']['jquery.ui.resizable'] = [
 				'dependencies' => [
@@ -225,6 +226,20 @@ class MapsSetup {
 			];
 
 			$this->mwGlobals['wgResourceModules']['jquery.ui.autocomplete'] = [
+				'dependencies' => [
+					'jquery.ui'
+				],
+				'targets' => [ 'desktop', 'mobile' ]
+			];
+
+			$this->mwGlobals['wgResourceModules']['jquery.ui.slider'] = [
+				'dependencies' => [
+					'jquery.ui'
+				],
+				'targets' => [ 'desktop', 'mobile' ]
+			];
+
+			$this->mwGlobals['wgResourceModules']['jquery.ui.dialog'] = [
 				'dependencies' => [
 					'jquery.ui'
 				],
