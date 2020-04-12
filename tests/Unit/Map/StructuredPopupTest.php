9ba2180e23f8f3227fe71484d5f3026ba6c4c8a9
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-04-12 07:33:20 +0200

    Add StructuredPopupTest

---
+++ tests/Unit/Map/StructuredPopupTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Maps\Tests\Unit\Map;
+
+use Maps\Map\StructuredPopup;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Maps\Map\StructuredPopup
+ */
+class StructuredPopupTest extends TestCase {
+
+	public function testWithoutPropertyValues() {
+		$this->assertSame(
+			'<h3 style="padding-top: 0">MyTitle</h3>',
+			$this->getHtml( 'MyTitle', [] )
+		);
+	}
+
+	private function getHtml( string $titleHtml, array $propertyValues ) {
+		return ( new StructuredPopup( $titleHtml, $propertyValues ) )->getHtml();
+	}
+
+	public function testTitleAndListAreSeparated() {
+		$this->assertContains(
+			'</h3><br><strong>',
+			$this->getHtml( 'MyTitle', [ 'P1' => 'V1' ] )
+		);
+	}
+
+	public function testListItems() {
+		$this->assertContains(
+			'<strong>P1</strong>: V1<br><strong>P2</strong>: V2',
+			$this->getHtml( 'MyTitle', [ 'P1' => 'V1', 'P2' => 'V2' ] )
+		);
+	}
+
+	public function testLinksAreAllowed() {
+		$this->assertContains(
+			'<strong><a href="#">P1</a></strong>: <a href="#">P1</a>',
+			$this->getHtml( 'MyTitle', [ '<a href="#">P1</a>' => '<a href="#">P1</a>' ] )
+		);
+	}
+
+	public function testImagesAreAllowed() {
+		$this->assertContains(
+			'<img src="#">',
+			$this->getHtml( 'MyTitle', [ 'P1' => '<img src="#">' ] )
+		);
+	}
+
+	public function testRandomTagsAreFiltered() {
+		$this->assertContains(
+			'<strong>P1</strong>: abcde',
+			$this->getHtml( 'MyTitle', [ 'P1<script>' => '<ul><li>abc</li></ul>de' ] )
+		);
+	}
+
+}
