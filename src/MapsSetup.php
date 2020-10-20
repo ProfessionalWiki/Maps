994a1a7f84e20d931be37305fa2260b56e153bf6
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-10-20 16:31:08 +0200

    Drop resource module compat code

--- src/MapsSetup.php
+++ src/MapsSetup.php
@@ -151,47 +151,6 @@ class MapsSetup {
 	}
 
 	private function registerEditApiModuleFallbacks() {
-		// mediawiki.api.edit is present in 1.31 but not 1.32
-		// Once Maps requires MW 1.32+, this can be removed after replacing usage of mediawiki.api.edit
-		if ( version_compare( $this->mwGlobals['wgVersion'], '1.32', '>=' ) ) {
-			$this->mwGlobals['wgResourceModules']['mediawiki.api.edit'] = [
-				'dependencies' => [
-					'mediawiki.api'
-				],
-				'targets' => [ 'desktop', 'mobile' ]
-			];
-		}
-
-		// 1.35 combines the jquery.ui modules into one
-		if ( version_compare( $this->mwGlobals['wgVersion'], '1.35', '>=' ) ) {
-			$this->mwGlobals['wgResourceModules']['jquery.ui.resizable'] = [
-				'dependencies' => [
-					'jquery.ui'
-				],
-				'targets' => [ 'desktop', 'mobile' ]
-			];
-
-			$this->mwGlobals['wgResourceModules']['jquery.ui.autocomplete'] = [
-				'dependencies' => [
-					'jquery.ui'
-				],
-				'targets' => [ 'desktop', 'mobile' ]
-			];
-
-			$this->mwGlobals['wgResourceModules']['jquery.ui.slider'] = [
-				'dependencies' => [
-					'jquery.ui'
-				],
-				'targets' => [ 'desktop', 'mobile' ]
-			];
-
-			$this->mwGlobals['wgResourceModules']['jquery.ui.dialog'] = [
-				'dependencies' => [
-					'jquery.ui'
-				],
-				'targets' => [ 'desktop', 'mobile' ]
-			];
-		}
 	}
 
 }
