ed3a01055c1a66de611d35c54eac1293f55bd675
alistair3149 <alistair31494322@gmail.com>
2025-03-04 17:00:32 -0500

    Fix tests

--- tests/Integration/Parser/LeafletTest.php
+++ tests/Integration/Parser/LeafletTest.php
@@ -15,8 +15,12 @@ class LeafletTest extends TestCase {
 		return TestFactory::newInstance()->parse( $textToParse );
 	}
 
+	private function assertStringContainsData( string $expected, string $html ): void {
+		$this->assertStringContainsString( htmlspecialchars( $expected ), $html );
+	}
+
 	public function testLeafletImageLayersIgnoresNotFoundImages() {
-		$this->assertStringContainsString(
+		$this->assertStringContainsData(
 			'"imageLayers":[]',
 			$this->parse(
 				"{{#leaflet:image layers=404.png}}"
@@ -25,7 +29,7 @@ class LeafletTest extends TestCase {
 	}
 
 	public function testLeafletImageLayersIgnoresImageUrls() {
-		$this->assertStringContainsString(
+		$this->assertStringContainsData(
 			'"imageLayers":[]',
 			$this->parse(
 				"{{#leaflet:image layers=https://user-images.githubusercontent.com/62098559/76514021-3fa9be80-647d-11ea-82ae-715420a5c432.png}}"
